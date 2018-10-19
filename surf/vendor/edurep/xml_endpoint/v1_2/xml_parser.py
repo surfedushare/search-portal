06199f351198f7c769f9b08feeff9efaf6c94cdf
ggevorg <gev.gevorkyan@gmail.com>
2018-10-19 13:13:12 +0300

    added parsing of drilldowns

--- surf/vendor/edurep/xml_endpoint/v1_2/xml_parser.py
+++ surf/vendor/edurep/xml_endpoint/v1_2/xml_parser.py
@@ -1,5 +1,7 @@
 from xml.etree import ElementTree as ET
 
+from surf.vendor.edurep.xml_endpoint.v1_2.choices import MIME_TYPE_TECH_FORMAT
+
 _NS = {
     "srw": "http://www.loc.gov/zing/srw/",
     "diag": "http://www.loc.gov/zing/srw/diagnostic/",
@@ -9,10 +11,12 @@ _NS = {
     "dd": "http://meresco.org/namespace/drilldown"
 }
 
+_TECH_FORMAT_LOM = "lom.technical.format"
+
 
 def parse_response(xml_text):
     root = ET.fromstring(xml_text)
-    return dict(number_of_records=_parse_number_of_records(root),
+    return dict(recordcount=_parse_number_of_records(root),
                 records=_parse_records(root),
                 drilldowns=_parse_drilldowns(root))
 
@@ -43,11 +47,27 @@ def _parse_drilldowns(root):
         drilldowns = drilldowns.find("dd:term-drilldown", namespaces=_NS)
         for dd in drilldowns:
             term_id = dd.attrib["name"]
-            for item in dd:
-                rv.setdefault(term_id, list()).append(
-                    dict(item_id=item.text, item_count=item.attrib["count"]))
+            if term_id == _TECH_FORMAT_LOM:
+                rv[term_id] = _parse_drilldowns_tech_format(dd)
+            else:
+                rv[term_id] = _parse_drilldowns_term(dd)
     except Exception:
         pass
-    return rv
+    return [dict(id=k, items=v) for k, v in rv.items()]
 
 
+def _parse_drilldowns_term(elem):
+    return [dict(id=item.text, count=int(item.attrib["count"]))
+            for item in elem]
+
+
+def _parse_drilldowns_tech_format(elem):
+    items = dict()
+    for item in elem:
+        mime_type = item.text
+        item_id = MIME_TYPE_TECH_FORMAT.get(mime_type)
+        if not item_id:
+            continue
+        items[item_id] = items.get(item_id, 0) + int(item.attrib["count"])
+
+    return [dict(id=k, count=v) for k, v in items.items()]
