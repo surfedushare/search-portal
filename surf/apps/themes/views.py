f027a366a621fc394969f4180b99f0f5ce0daccf
Gevorg Gevorkyan <gevorg@moqod.com>
2018-11-17 18:09:54 +0300

    added api method to get theme disciplines

--- surf/apps/themes/views.py
+++ surf/apps/themes/views.py
@@ -12,6 +12,7 @@ from rest_framework.response import Response
 
 from surf.apps.themes.models import Theme
 from surf.apps.themes.serializers import ThemeSerializer
+from surf.apps.filters.serializers import FilterCategoryItemSerializer
 
 
 class ThemeViewSet(ListModelMixin,
@@ -25,3 +26,14 @@ class ThemeViewSet(ListModelMixin,
     serializer_class = ThemeSerializer
     permission_classes = []
     lookup_field = 'filter_category_item__id'
+
+    @action(methods=['get'], detail=True)
+    def disciplines(self, request, pk=None, **kwargs):
+        instance = self.get_object()
+
+        res = []
+        if instance.disciplines.exists():
+            res = FilterCategoryItemSerializer(many=True).to_representation(
+                instance.disciplines.all())
+
+        return Response(res)
