87cda16d3444124f1ce1cbd76279ea46df1b7b31
ggevorg <gev.gevorkyan@gmail.com>
2018-12-05 18:25:12 +0300

    added action to admin page to fill Material extra data

--- surf/apps/materials/admin.py
+++ surf/apps/materials/admin.py
@@ -3,6 +3,7 @@ from django import forms
 
 from surf.apps.materials import models
 from surf.apps.filters.models import FilterCategoryItem
+from surf.apps.materials.utils import update_materials_data
 from surf.vendor.edurep.xml_endpoint.v1_2.api import DISCIPLINE_FIELD_ID
 
 
@@ -23,9 +24,23 @@ class MaterialForm(forms.ModelForm):
         fields = '__all__'
 
 
+def fill_material_data(model_admin, request, queryset):
+    update_materials_data(queryset.all())
+
+
+fill_material_data.short_description = "Fill material data"
+
+
 @admin.register(models.Material)
 class MaterialAdmin(admin.ModelAdmin):
+    actions = [fill_material_data]
     form = MaterialForm
+
+    def get_actions(self, request):
+        actions = super().get_actions(request)
+        if not request.user.is_superuser:
+            actions.pop('fill_material_data', None)
+        return actions
 
 
 @admin.register(models.Collection)
---
+++ surf/apps/materials/utils.py
@@ -0,0 +1,33 @@
+import json
+
+from django.conf import settings
+
+from surf.vendor.edurep.xml_endpoint.v1_2.api import XmlEndpointApiClient
+
+
+def update_materials_data(materials):
+    """
+    Updates material extra data from EduRep
+    :param materials: list of material DB instances
+    """
+
+    ac = XmlEndpointApiClient(
+        api_endpoint=settings.EDUREP_XML_API_ENDPOINT)
+
+    for material in materials:
+        res = ac.get_materials_by_id(['"{}"'.format(material.external_id)])
+        if not res:
+            continue
+
+        try:
+            m = res.get("records", [])[0]
+            material.material_url = m.get("url")
+            material.title = m.get("title")
+            material.description = m.get("description")
+            keywords = m.get("keywords")
+            if keywords:
+                keywords = json.dumps(keywords)
+            material.keywords = keywords
+            material.save()
+        except IndexError:
+            pass
