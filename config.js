0a66533a31e222387b780c619258513bc4934b52
Yurii Dunaev <ukolsd@gmail.com>
2019-02-25 16:11:22 +0400

    Config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+/**
+ * Created by urijdunaev on 2019-02-25.
+ */
+
+'use strict';
+
+export const LOGOUT_URL = 'https://engine.test.surfconext.nl/logout';
+export const BASE_URL = 'https://surf.stg.mqd.me/api/v1/';
+export const PROD_URL = 'https://surfcatalog.firebaseapp.com';
