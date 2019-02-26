995695d7c47e150be610e044d1ad40cd77a16146
Yurii Dunaev <ukolsd@gmail.com>
2019-02-26 13:45:35 +0400

    Fixing URLs

--- config.js
+++ config.js
@@ -5,5 +5,5 @@
 'use strict';
 
 export const LOGOUT_URL = 'https://engine.test.surfconext.nl/logout';
-export const BASE_URL = 'https://back-dev.surfcatalog.nl/api/v1/';
+export const BASE_URL = 'https://back-dev.surfcatalog.nl/api/v1';
 export const PROD_URL = 'https://front-dev.surfcatalog.nl/';
