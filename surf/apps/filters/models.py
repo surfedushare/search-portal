de17cbece3a2e31d29ea86ae06f118be033d0f7d
Jeroen van Hoek <jeroen.van.hoek@quandago.com>
2019-08-30 16:27:37 +0200

    Add mpttfilteritem to filteritems before deleting

--- surf/apps/filters/models.py
+++ surf/apps/filters/models.py
@@ -61,6 +61,41 @@ class FilterCategoryItem(UUIDModel):
     def __str__(self):
         return self.title
 
+class MpttFilterItem(MPTTModel, UUIDModel):
+    name = django_models.CharField(max_length=255)
+    parent = TreeForeignKey('self', on_delete=django_models.CASCADE, null=True, blank=True, related_name='children')
+
+    created_at = django_models.DateTimeField(auto_now_add=True)
+    updated_at = django_models.DateTimeField(auto_now=True)
+    deleted_from_edurep_at = django_models.DateTimeField(default=None, null=True, blank=True)
+
+    title_translations = django_models.OneToOneField(to=Locale, on_delete=django_models.CASCADE, null=True, blank=False)
+    external_id = django_models.CharField(max_length=255, verbose_name="Field id in EduRep", blank=True)
+    enabled_by_default = django_models.BooleanField(default=False)
+    is_hidden = django_models.BooleanField(default=False)
+
+    item_count = 0
+
+    def __str__(self):
+        return self.name
+
+    def to_dict(self):
+        return {
+            "name": self.name,
+            "created_at": self.created_at.strftime('%c'),
+            "updated_at": self.updated_at.strftime('%c'),
+            "title_translations": self.title_translations.toJSON() if self.title_translations else None,
+            "external_id": self.external_id,
+            "enabled_by_default": self.enabled_by_default,
+            "is_hidden": self.is_hidden,
+        }
+
+    def toJSON(self):
+        return json.dumps(self.to_dict())
+
+    class MPTTMeta:
+        order_insertion_by = ['name']
+
 
 class Filter(UUIDModel):
     """
@@ -102,42 +137,11 @@ class FilterItem(UUIDModel):
     category_item = django_models.ForeignKey(FilterCategoryItem,
                                              related_name='filter_items',
                                              on_delete=django_models.CASCADE)
+    # Filter category item related to this item
+    mptt_category_item = django_models.ForeignKey(MpttFilterItem,
+                                             related_name='filter_items',
+                                             on_delete=django_models.CASCADE,
+                                                  null=True)
 
     def __str__(self):
         return "{} - {}".format(self.filter.title, self.category_item.title)
-
-
-class MpttFilterItem(MPTTModel, UUIDModel):
-    name = django_models.CharField(max_length=255)
-    parent = TreeForeignKey('self', on_delete=django_models.CASCADE, null=True, blank=True, related_name='children')
-
-    created_at = django_models.DateTimeField(auto_now_add=True)
-    updated_at = django_models.DateTimeField(auto_now=True)
-    deleted_from_edurep_at = django_models.DateTimeField(default=None, null=True, blank=True)
-
-    title_translations = django_models.OneToOneField(to=Locale, on_delete=django_models.CASCADE, null=True, blank=False)
-    external_id = django_models.CharField(max_length=255, verbose_name="Field id in EduRep", blank=True)
-    enabled_by_default = django_models.BooleanField(default=False)
-    is_hidden = django_models.BooleanField(default=False)
-
-    item_count = 0
-
-    def __str__(self):
-        return self.name
-
-    def to_dict(self):
-        return {
-            "name": self.name,
-            "created_at": self.created_at.strftime('%c'),
-            "updated_at": self.updated_at.strftime('%c'),
-            "title_translations": self.title_translations.toJSON() if self.title_translations else None,
-            "external_id": self.external_id,
-            "enabled_by_default": self.enabled_by_default,
-            "is_hidden": self.is_hidden,
-        }
-
-    def toJSON(self):
-        return json.dumps(self.to_dict())
-
-    class MPTTMeta:
-        order_insertion_by = ['name']
