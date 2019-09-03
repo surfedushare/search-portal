a021d7178718438e204db812f9691edfa026cb2d
Fako Berkers <email@fakoberkers.nl>
2019-09-03 08:39:04 +0200

    Adds endpoints for translations

--- surf/urls.py
+++ surf/urls.py
@@ -13,11 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf import settings
 from django.contrib import admin
-from django.urls import path
 from django.conf.urls import url, include
-from django.conf.urls.static import static
+
 
 from surf.routers import CustomRouter
 
@@ -47,6 +45,7 @@ from surf.apps.users.views import (
 from surf.apps.communities.views import CommunityViewSet
 from surf.apps.themes.views import ThemeViewSet
 from surf.apps.stats.views import StatsView
+from surf.apps.locale.views import get_localisation_strings
 
 admin.site.site_header = 'Surf'
 admin.site.site_title = 'Surf'
@@ -81,4 +80,6 @@ urlpatterns = [
 
     url(r'^login/surfconext/', auth_begin_handler),
     url(r'^complete/surfconext/', auth_complete_handler),
+
+    url(r'^locales/(?P<locale>en|nl)/?$', get_localisation_strings),
 ]
