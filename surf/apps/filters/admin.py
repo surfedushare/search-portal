569bf22df3c0013c8590797ef4dae235a91db408
Jeroen van Hoek <jeroen.van.hoek@quandago.com>
2019-09-05 16:53:13 +0200

    Removes commented-out admin code

--- surf/apps/filters/admin.py
+++ surf/apps/filters/admin.py
@@ -43,35 +43,6 @@ def fill_filter_category(model_admin, request, queryset):
 fill_filter_category.short_description = "Fill filter category items"
 
 
-# @admin.register(models.FilterCategory)
-# class FilterCategoryAdmin(admin.ModelAdmin):
-#     """
-#     Provides admin options and functionality for FilterCategory model.
-#     """
-#
-#     actions = [fill_filter_category]
-#
-#     list_display = ("title", "edurep_field_id", "max_item_count", )
-#     ordering = ("title", )
-#
-#     def get_actions(self, request):
-#         actions = super().get_actions(request)
-#         if not request.user.is_superuser:
-#             actions.pop('fill_filter_category', None)
-#         return actions
-
-
-# @admin.register(models.FilterCategoryItem)
-# class FilterCategoryItemAdmin(admin.ModelAdmin):
-#     """
-#     Provides admin options and functionality for FilterCategoryItem model.
-#     """
-#
-#     list_display = ("title", "external_id", "category", "order",)
-#     ordering = ("title", )
-#     list_filter = ("category",)
-
-
 @admin.register(models.MpttFilterItem)
 class MpttFilterItemAdmin(DraggableMPTTAdmin):
 
