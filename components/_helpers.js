0bd3b41bd641928f39fc9bfed079ff88a5cb0888
Fako Berkers <email@fakoberkers.nl>
2019-09-26 17:44:46 +0200

    Updates paths to be Dutch where needed

--- components/_helpers.js
+++ components/_helpers.js
@@ -17,18 +17,13 @@ export const debounce = function(func, wait, immediate) {
   };
 };
 
-export const generateSearchMaterialsQuery = function(data = { filters: [], search_text: [] }, path = '/materials/search/') {
+export const generateSearchMaterialsQuery = function(data = { filters: [], search_text: [] }, name = 'materials-search') {
 
-  const filters = data.filters
-    ? data.filters.filter(item => Object.keys(item).length)
-    : [];
-
-  if (this && this.$i18n.locale !== 'nl') {
-    path = `/${this.$i18n.locale}${path}`;
-  }
+  const filters = data.filters ? data.filters.filter(item => Object.keys(item).length) : [];
+  name += '___' + this.$i18n.locale;
 
   return {
-    path: path,
+    name: name,
     query: Object.assign({}, data, {
       filters: JSON.stringify(filters),
       search_text: JSON.stringify(data.search_text)
