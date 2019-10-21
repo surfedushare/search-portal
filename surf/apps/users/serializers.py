7a9ff87f9a74ef681175f039a45b04c1950568aa
Jeroen van Hoek <jeroen.van.hoek@quandago.com>
2019-10-21 14:23:04 +0200

    Add communities & collections to user/me

--- surf/apps/users/serializers.py
+++ surf/apps/users/serializers.py
@@ -5,6 +5,7 @@ This module contains API view serializers for users app.
 from rest_framework import serializers
 
 from surf.apps.users.models import User
+from surf.apps.communities.models import Community
 
 
 class UserDetailsSerializer(serializers.ModelSerializer):
@@ -14,14 +15,28 @@ class UserDetailsSerializer(serializers.ModelSerializer):
 
     full_name = serializers.CharField(source="surfconext_auth.display_name")
     is_admin = serializers.SerializerMethodField()
+    communities = serializers.SerializerMethodField()
+    collections = serializers.SerializerMethodField()
+
+    @staticmethod
+    def get_communities(obj):
+        qs = Community.objects.filter(team__user=obj)
+        ids = qs.values_list('id', flat=True)
+        return ids
+
+    @staticmethod
+    def get_collections(obj):
+        qs = Community.objects.filter(team__user=obj)
+        ids = qs.values_list('collections__id', flat=True)
+        return list(set(ids))
 
     @staticmethod
     def get_is_admin(obj):
         if not obj.admin_teams:
             return False
 
-        return obj.admin_teams.filter(community__isnull=False).exists()
+        return obj.admin_teams.exists()
 
     class Meta:
         model = User
-        fields = ('id', 'full_name', 'is_admin',)
+        fields = ('id', 'full_name', 'is_admin', 'communities', 'collections')
