faee8f9f71ef87923f4596eefd70c51c228bafb3
Jeroen van Hoek <jeroen.van.hoek@quandago.com>
2019-10-21 12:00:35 +0200

    Check whether external_id is empty before regex checking

--- surf/apps/communities/models.py
+++ surf/apps/communities/models.py
@@ -124,10 +124,11 @@ class Community(UUIDModel):
         # Check whether the entered external_id is a valid URN.
         # regex taken from the O'Reilly Regular Expressions Cookbook, 2nd Edition
         # https://www.oreilly.com/library/view/regular-expressions-cookbook/9781449327453/ch08s06.html
-        regex = r"^urn:[a-z0-9][a-z0-9-]{0,31}:[a-z0-9()+,\-.:=@;$_!*'%/?#]+$"
-        if not re.match(regex, self.external_id):
-            raise ValidationError("SURFconext group id isn't a valid URN. Check "
-                                  "https://en.wikipedia.org/wiki/Uniform_Resource_Name for examples of valid URNs.")
+        if self.external_id:
+            regex = r"^urn:[a-z0-9][a-z0-9-]{0,31}:[a-z0-9()+,\-.:=@;$_!*'%/?#]+$"
+            if not re.match(regex, self.external_id):
+                raise ValidationError("SURFconext group id isn't a valid URN. Check "
+                                      "https://en.wikipedia.org/wiki/Uniform_Resource_Name for examples of valid URNs.")
 
 
 class Team(django_models.Model):
