7e77b992052e79947fd082c2621d5f28f854bcad
Fako Berkers <email@fakoberkers.nl>
2019-10-21 13:03:54 +0200

    Removes remaining validation code from removed validation library (due to file size)

--- components/InputFile/InputFile.component.js
+++ components/InputFile/InputFile.component.js
@@ -27,27 +27,23 @@ export default {
      * @param e - Event
      */
     onFileChange(e) {
-      if (!this.errors.has('file')) {
-        let files = e.target.files || e.dataTransfer.files;
-        if (!files.length) return;
-        this.createImage(files[0]);
-        this.imageText = files[0].name;
-      }
+      let files = e.target.files || e.dataTransfer.files;
+      if (!files.length) return;
+      this.createImage(files[0]);
+      this.imageText = files[0].name;
     },
     /**
      * Create image
      * @param file
      */
     createImage(file) {
-      if (!this.errors.has('file')) {
-        let reader = new FileReader();
-        let that = this;
+      let reader = new FileReader();
+      let that = this;
 
-        reader.onload = e => {
-          that.image = e.target.result;
-        };
-        reader.readAsDataURL(file);
-      }
+      reader.onload = e => {
+        that.image = e.target.result;
+      };
+      reader.readAsDataURL(file);
     },
     /**
      * Remove image
