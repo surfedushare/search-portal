a3902d6eb8c13b5946b36a7d720b2d9c14d4ae45
Fako Berkers <email@fakoberkers.nl>
2019-10-24 17:19:15 +0200

    Adds a missing file

---
+++ pages/my/communities.vue
@@ -0,0 +1,15 @@
+<template>
+  <section class="container main">
+    <Communities user-communities="true" />
+  </section>
+</template>
+
+<script>
+import Communities from '~/components/Communities';
+
+export default {
+  components: {
+    Communities
+  }
+};
+</script>
