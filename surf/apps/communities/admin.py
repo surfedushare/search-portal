5690ae7542217fcff736b3c23d3ac7d01b5eb166
Jeroen van Hoek <jeroen.van.hoek@quandago.com>
2019-11-04 10:34:54 +0100

    Add deleted_at to materials, collections & communities

--- surf/apps/communities/admin.py
+++ surf/apps/communities/admin.py
@@ -10,6 +10,42 @@ from surf.apps.communities import models
 from surf.apps.communities.models import PublishStatus
 
 
+def trash_nodes(modeladmin, request, queryset):
+    for obj in queryset:
+        obj.delete()
+
+
+trash_nodes.short_description = "Trash selected %(verbose_name_plural)s"
+
+
+def restore_nodes(modeladmin, request, queryset):
+    for obj in queryset:
+        obj.restore()
+
+
+restore_nodes.short_description = "Restore selected %(verbose_name_plural)s"
+
+
+class TrashListFilter(admin.SimpleListFilter):
+
+    title = 'is trash'
+    parameter_name = 'trash'
+
+    def lookups(self, request, model_admin):
+        return (
+            ('1', 'Yes'),
+            ('0', 'No'),
+        )
+
+    def queryset(self, request, queryset):
+        value = self.value() or '0'
+        try:
+            is_trash = bool(int(value))
+        except ValueError:
+            is_trash = False
+        return queryset.filter(deleted_at__isnull=not is_trash)
+
+
 class CommunityForm(forms.ModelForm):
     """
     Implementation of Community Form class.
@@ -56,9 +92,25 @@ class CommunityAdmin(admin.ModelAdmin):
     Provides admin options and functionality for Community model.
     """
     list_display = ("name", "publish_status",)
-    list_filter = ("publish_status",)
+    list_filter = ("publish_status", TrashListFilter,)
+    readonly_fields = ("deleted_at",)
     inlines = [TeamInline]
     form = CommunityForm
+
+    actions = [restore_nodes, trash_nodes]
+
+    def get_actions(self, request):
+        actions = super().get_actions(request)
+        try:
+            filter_trash = bool(int(request.GET.get('trash', '0')))
+        except ValueError:
+            filter_trash = False
+        if filter_trash:
+            del actions["trash_nodes"]
+        else:
+            del actions["delete_selected"]
+            del actions["restore_nodes"]
+        return actions
 
 
 @admin.register(models.SurfTeam)
