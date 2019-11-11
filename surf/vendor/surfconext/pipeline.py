d2a3a94e10260633e94c722fbd7bd39d5c14fc1f
Fako Berkers <email@fakoberkers.nl>
2019-11-11 14:08:29 +0100

    Updates the pipeline to properly include groups in user details

--- surf/vendor/surfconext/pipeline.py
+++ surf/vendor/surfconext/pipeline.py
@@ -1,5 +1,7 @@
 from django.conf import settings
 
+from sentry_sdk import capture_message
+
 from surf.vendor.surfconext.voot.api import VootApiClient
 from social_core.pipeline.partial import partial
 
@@ -7,8 +9,9 @@ from social_core.pipeline.partial import partial
 @partial
 def require_data_permissions(strategy, details, user=None, is_new=False, *args, **kwargs):
     return
+    # TODO: enable this code to check user permissions half way the login procedure
     # This will need: permission validation
-    # NB: this raises 500 when a partial state gets used twice (which should not happen)
+    # NB: this raises 500 when a partial state gets used twice (an event which should not occur but still)
     if kwargs.get('ajax') or user and user.email:
         return
     elif is_new and not details.get('permissions'):
@@ -23,9 +26,10 @@ def require_data_permissions(strategy, details, user=None, is_new=False, *args, 
             )
 
 
-def get_groups(strategy, response, *args, **kwargs):
+def get_groups(strategy, details, response, *args, **kwargs):
     vac = VootApiClient(api_endpoint=settings.VOOT_API_ENDPOINT)
     groups = vac.get_groups(response.get("access_token"))
     if not isinstance(groups, list):
-        raise TypeError(f"VootApiClient didn't return a list but returned \"{groups}\" instead.")
-    print(groups)
+        capture_message(f"VootApiClient didn't return a list but returned \"{groups}\" instead.")
+        groups = []
+    details["groups"] = groups
