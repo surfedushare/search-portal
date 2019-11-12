38245a011210aaf94962dddb909336d40b2842f2
Fako Berkers <email@fakoberkers.nl>
2019-11-12 10:09:08 +0100

    Cleans the code a bit

--- surf/apps/users/admin.py
+++ surf/apps/users/admin.py
@@ -1,7 +1,3 @@
-"""
-This module provides django admin functionality for users app.
-"""
-
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
 
@@ -10,7 +6,4 @@ from surf.apps.users import models
 
 @admin.register(models.User)
 class UserAdmin(DjangoUserAdmin):
-    """
-    Provides admin options and functionality for User model.
-    """
     pass
