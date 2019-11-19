118be2a98bcc2efd9ee81a534beda436851558bf
Fako Berkers <email@fakoberkers.nl>
2019-11-19 16:16:47 +0100

    Fetches the user when not logged in

--- src/plugins/auth.js
+++ src/plugins/auth.js
@@ -2,5 +2,7 @@ export default function({ store }) {
   const token = localStorage.getItem('surf_token');
   if (token) {
     store.dispatch('authenticate', { token });
+  } else {
+    store.dispatch('getUser');
   }
 }
