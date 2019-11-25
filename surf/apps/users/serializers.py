9e281eb23cc7f3e546cb6daeb63280465938d231
Fako Berkers <email@fakoberkers.nl>
2019-11-25 12:53:19 +0100

    Orders collections by title

--- surf/apps/users/serializers.py
+++ surf/apps/users/serializers.py
@@ -28,7 +28,7 @@ class UserDetailsSerializer(serializers.ModelSerializer):
 
     @staticmethod
     def get_collections(obj):
-        qs = Collection.objects.filter(communities__team__user=obj)
+        qs = Collection.objects.filter(communities__team__user=obj).order_by("title")
         return CollectionShortSerializer(qs, many=True).data
 
     @staticmethod
