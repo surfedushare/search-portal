2e639f32c18644aef009e2729c3e03e5e2d7c162
Fako Berkers <email@fakoberkers.nl>
2020-03-26 10:22:55 +0100

    Fixes alignment of communities on overview pages

--- src/pages/communities.vue
+++ src/pages/communities.vue
@@ -203,7 +203,7 @@
     &__items {
       width: 100%;
       display: flex;
-      justify-content: space-between;
+      justify-content: left;
       flex-wrap: wrap;
       padding: 0;
       list-style: none;
