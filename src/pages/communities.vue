870d9b2dfad2c275d96cd6f8f3d7426b191c701c
Fako Berkers <email@fakoberkers.nl>
2020-04-01 15:29:18 +0200

    Fixes bug with community translations

--- src/pages/communities.vue
+++ src/pages/communities.vue
@@ -44,7 +44,8 @@
             </router-link>
           </li>
         </ul>
-        <h3 class="text-center" v-else v-html="$t('html-No-communities-available')"></h3>
+        <h3 class="text-center" v-else-if="userCommunities" v-html="$t('html-No-communities-available-logged-in')"></h3>
+        <h3 class="text-center" v-else>{{ $t('No-communities-available') }}</h3>
       </div>
     </section>
   </section>
