67e5256aef06c51860057861c15effa66437d451
Fako Berkers <email@fakoberkers.nl>
2020-04-02 13:51:01 +0200

    Allows selection in the collection component

--- src/components/Collections/Collections.component.js
+++ src/components/Collections/Collections.component.js
@@ -20,11 +20,28 @@ export default {
       default: false
     }
   },
+  data() {
+    return {
+      selection: []
+    }
+  },
   components: { Spinner },
   methods: {
     getCommunityDetail(community, language, detail) {
       let communityDetails = _.find(community.community_details, {language_code: language.toUpperCase()});
       return communityDetails[detail] || null
+    },
+    selectCollection(collection) {
+      collection.selected = !collection.selected;
+      if (this.selection.indexOf(collection.id) === -1) {
+        this.selection.push(collection.id);
+      } else {
+        this.selection = this.selection.filter(
+          item => item !== collection.id
+        );
+      }
+      this.$emit('input', this.selection);
+      this.$forceUpdate();
     }
   }
 };
