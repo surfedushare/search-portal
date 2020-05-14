a35a63c871dbdbd3544cc760bf46a3f57b116712
Kirsten Ruys <kirsten.ruys@gmail.com>
2020-05-14 10:29:10 +0200

    Upload media to S3 bucket

--- service/surf/settings/base.py
+++ service/surf/settings/base.py
@@ -245,9 +245,12 @@ if MODE == "container" and DEBUG:
         os.path.join('/usr/src/static')
     ]
 
-MEDIA_ROOT = os.path.join(BASE_DIR, '..', '..', 'media')
-MEDIA_URL = '/media/'
-
+# AWS credentials for media upload to S3 bucket 'search-portal-media-uploads'
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID', '')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY', '')
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+AWS_STORAGE_BUCKET_NAME = 'search-portal-media-uploads'
+AWS_S3_REGION_NAME = 'eu-central-1'
 
 # Django Webpack loader
 # https://github.com/owais/django-webpack-loader
--- service/surf/urls.py
+++ service/surf/urls.py
@@ -85,8 +85,6 @@ urlpatterns = [
     url(r'^.*/$', portal_single_page_application),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)  # ignored in production
-
 if settings.DEBUG:
     import debug_toolbar
     urlpatterns = [
