86f66b200c865a185db5856167257abc800be8a0
Kirsten Ruys <kirsten.ruys@gmail.com>
2020-05-26 10:16:58 +0200

    github actions settings in service

--- service/surf/settings/base.py
+++ service/surf/settings/base.py
@@ -176,6 +176,18 @@ DATABASES = {
     }
 }
 
+if os.environ.get('GITHUB_WORKFLOW'):
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'github_actions',
+            'USER': 'postgres',
+            'PASSWORD': 'postgres',
+            'HOST': '127.0.0.1',
+            'PORT': 5432,
+        }
+    }
+
 
 # Django Rest Framework
 # https://www.django-rest-framework.org/
