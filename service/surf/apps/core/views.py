ff0620fb426f77e85a0dadd2dd85cad45e07d02a
Fako Berkers <email@fakoberkers.nl>
2020-05-27 14:22:31 +0200

    Gets package info from settings not packaging module directly

--- service/surf/apps/core/views.py
+++ service/surf/apps/core/views.py
@@ -1,9 +1,8 @@
+from django.conf import settings
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import api_view, schema, permission_classes
 from rest_framework.permissions import AllowAny
-
-from packaging import get_package_info
 
 
 @api_view()
@@ -11,6 +10,5 @@ from packaging import get_package_info
 @schema(None)
 def health_check(request):
     data = {"healthy" : True}
-    package_info = get_package_info()
-    data.update(package_info)
+    data.update(settings.PACKAGE_INFO)
     return Response(data, status.HTTP_200_OK)
