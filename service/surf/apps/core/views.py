8b16d9689a470e0a137c06500deba3668f7a5c88
Fako Berkers <email@fakoberkers.nl>
2020-05-27 12:57:52 +0200

    Adds version information to the health check

--- service/surf/apps/core/views.py
+++ service/surf/apps/core/views.py
@@ -3,9 +3,14 @@ from rest_framework import status
 from rest_framework.decorators import api_view, schema, permission_classes
 from rest_framework.permissions import AllowAny
 
+from packaging import get_package_info
+
 
 @api_view()
 @permission_classes([AllowAny])
 @schema(None)
 def health_check(request):
-    return Response({"healthy" : True}, status.HTTP_200_OK)
+    data = {"healthy" : True}
+    package_info = get_package_info()
+    data.update(package_info)
+    return Response(data, status.HTTP_200_OK)
