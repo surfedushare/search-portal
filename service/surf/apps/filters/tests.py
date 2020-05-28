7259d0e0e7dc107c6a08aed004e842cb28c98970
Kirsten Ruys <kirsten.ruys@gmail.com>
2020-05-28 16:47:27 +0200

    Filter categories test

--- service/surf/apps/filters/tests.py
+++ service/surf/apps/filters/tests.py
@@ -9,4 +9,9 @@ class TestFilters(TestCase):
     def test_filter_categories(self):
         client = Client()
         response = client.get("/api/v1/filter-categories/")
+        count = response.json()['count']
+        results = response.json()['results']
+        names = [result['name'] for result in results]
         self.assertEqual(response.status_code, 200)
+        self.assertEqual(count, 9)
+        self.assertEqual(names, ['Bron', 'Bruikbaar als', 'Gebruiksrechten', 'Onderwijsniveau', 'Publicatiedatum', 'Soort materiaal', 'Taal', 'Thema', 'Vakgebied'])
