ffb844955eb43a85376818d8116368349a4c5e37
Kirsten Ruys <kirsten.ruys@gmail.com>
2020-06-08 14:25:05 +0200

    print url

--- service/surf/apps/materials/tests/basic_search.py
+++ service/surf/apps/materials/tests/basic_search.py
@@ -21,5 +21,6 @@ class TestBasicSearch(StaticLiveServerTestCase):
 
     def test_home_page(self):
         self.selenium.get(self.live_server_url)
+        print(self.live_server_url)
         self.selenium.find_element_by_css_selector(".search.main__info_search input[type=search]")
         self.selenium.find_element_by_css_selector(".search.main__info_search button")
