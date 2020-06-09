f72f11a9bfa9126eb7e4a3b2fae2bd2d84893760
Kirsten Ruys <kirsten.ruys@gmail.com>
2020-06-09 15:46:45 +0200

    Locale tests

--- service/surf/apps/locale/tests.py
+++ service/surf/apps/locale/tests.py
@@ -1,3 +1,29 @@
 from django.test import TestCase
+from django.test import Client
 
-# Create your tests here.
+
+class TestLocales(TestCase):
+
+    fixtures = ['locales']
+
+    def test_locales_en(self):
+        client = Client()
+        response = client.get("/locales/en")
+        results = response.json()
+        selections = results["My-selections"]
+        logout = results["logout"]
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(selections, "My selections")
+        self.assertEqual(logout, "Log out")
+
+    def test_locales_nl(self):
+        client = Client()
+        response = client.get("/locales/nl")
+        results = response.json()
+        selections = results["My-selections"]
+        logout = results["logout"]
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(selections, "Mijn selecties")
+        self.assertEqual(logout, "Uitloggen")
