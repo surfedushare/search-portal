eff22098964bd5ac6369cbc3be3da4ee599263a4
Fako Berkers <email@fakoberkers.nl>
2020-07-02 14:58:05 +0200

    Fixes containers for starting commands in background

--- harvester/core/background.py
+++ harvester/core/background.py
@@ -1,7 +1,7 @@
 import logging
 from invoke import Context
 
-#from harvester.tasks_local import import_dataset
+from tasks_local import import_dataset
 from harvester.settings import environment
 from harvester.celery import app
 
@@ -14,7 +14,7 @@ def health_check():
     log.info(f"Healthy: {environment.django.domain}")
 
 
-# @app.task(name="import_dataset")
-# def celery_import_dataset(dataset):
-#     ctx = Context(environment)
-#     import_dataset(ctx, dataset)
+@app.task(name="import_dataset")
+def celery_import_dataset(dataset):
+    ctx = Context(environment)
+    import_dataset(ctx, dataset, use_aws_profile=False)
