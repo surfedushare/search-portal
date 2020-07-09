f8997dd7bf03a1c02d6afeff31033371ae4da8cc
Ruben-Hartog <rhartog@zilverline.com>
2020-07-09 09:45:02 +0200

    Remove search from InfoBlock, it isn't used

--- portal/src/components/InfoBlock/InfoBlock.js
+++ portal/src/components/InfoBlock/InfoBlock.js
@@ -1,7 +1,3 @@
-import { mapGetters } from 'vuex'
-import Search from '~/components/Search'
-import Materials from '~/components/Materials'
-
 export default {
   props: {
     title: '',
@@ -9,19 +5,5 @@ export default {
     logo_src: { default: '/images/pictures/image_home.jpg', type: String },
     featured_image: '',
     website_url: ''
-  },
-  components: {
-    Search,
-    Materials
-  },
-  computed: {
-    ...mapGetters(['materials'])
-  },
-  mounted() {
-    this.$store.dispatch('searchMaterials', {
-      page_size: 4,
-      search_text: '',
-      return_filters: false
-    })
   }
 }
