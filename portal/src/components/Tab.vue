cbc6408984b6a33e10c3ecb440618c61f0d63897
Ruben-Hartog <rhartog@zilverline.com>
2020-07-23 09:12:12 +0200

    Create reusable tab components

---
+++ portal/src/components/Tab.vue
@@ -0,0 +1,27 @@
+<template>
+  <div v-show="isActive" class="tabcontent">
+    <slot></slot>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Tab',
+  props: {
+    title: {
+      type: String,
+      default: ''
+    }
+  },
+  data() {
+    return {
+      isActive: true
+    }
+  }
+}
+</script>
+<style lang="less" scoped>
+.tabcontent {
+  padding: 6px 12px;
+  border-top: none;
+}
+</style>
