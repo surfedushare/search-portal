4c06c472c811433a671852b9ab9b16b55a47dcde
Fako Berkers <email@fakoberkers.nl>
2020-09-10 10:41:32 +0200

    Updates the harvester documentation

--- environments/surfpol/packaging.py
+++ environments/surfpol/packaging.py
@@ -9,7 +9,7 @@ logger = logging.getLogger(__file__)
 def get_package_info():
     info_file_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), "info.json")
     if not os.path.exists(info_file_path):
-        logger.warning("Can't find info.json in environment directory. Perhaps 'invoke prepare-build' will help?")
+        logger.warning("Can't find info.json in environment directory. Perhaps 'invoke prepare-builds' will help?")
         return {"versions": {}}
     with open(info_file_path) as info_file:
         info = json.load(info_file)
