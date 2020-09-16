5991a2b3283a5489bcb72b521e9d5f306a3dbea7
Fako Berkers <email@fakoberkers.nl>
2020-09-16 09:08:56 +0200

    Switches the harvest task to get seeds from Edurep

--- harvester/harvester/background.py
+++ harvester/harvester/background.py
@@ -1,5 +1,7 @@
 import logging
+from invoke import Context
 
+from django.conf import settings
 from django.core.management import call_command
 
 from harvester.settings import environment
@@ -25,10 +27,6 @@ def celery_import_dataset(dataset, role="primary"):
 
 @app.task(name="harvest")
 def harvest(role="primary", reset=False):
-    # TODO: to limit resource use Edurep asked us to only sync with them once a day
-    # We can facilitate this by loading the production OAI-PMH resources on development and acceptance.
-    # We'll check the role and if it's not primary skip download.
-    # However as we are developing I'm skipping this for now.
 
     # Iterate over all active datasets to get data updates
     for dataset in Dataset.objects.filter(is_active=True):
@@ -39,16 +37,23 @@ def harvest(role="primary", reset=False):
             dataset.reset()
         # First we call the commands that will query the OAI-PMH interfaces
         if role == "primary":
-            # TODO: enable this line for "primary" once the NAT gateway on AWS passes on internet
-            # call_command("harvest_edurep_seeds", f"--dataset={dataset.name}")
-            # TODO: this command should only be run if role is not primary once everything is migrated to AWS
+            call_command("harvest_edurep_seeds", f"--dataset={dataset.name}")
+        else:
             call_command("load_edurep_oaipmh_data", "--force-download")
-            # After getting all the metadata we'll download content
-            call_command("harvest_basic_content", f"--dataset={dataset.name}")
-            # We skip any video downloading/processing for now
-            # Later we want YoutubeDL to download the videos and Amber to process them
-            OAIPMHHarvest.objects.filter(stage=HarvestStages.BASIC).update(stage=HarvestStages.VIDEO)
-            # Aggregating the metadata and content into the dataset
-            call_command("update_dataset", f"--dataset={dataset.name}")
-            # Based on the dataset we push to Elastic Search
-            call_command("push_es_index", f"--dataset={dataset.name}")
+        # After getting all the metadata we'll download content
+        call_command("harvest_basic_content", f"--dataset={dataset.name}")
+        # We skip any video downloading/processing for now
+        # Later we want YoutubeDL to download the videos and Amber to process them
+        OAIPMHHarvest.objects.filter(stage=HarvestStages.BASIC).update(stage=HarvestStages.VIDEO)
+        # Aggregating the metadata and content into the dataset
+        call_command("update_dataset", f"--dataset={dataset.name}")
+        # Based on the dataset we push to Elastic Search
+        call_command("push_es_index", f"--dataset={dataset.name}")
+
+    # After processing all active datasets we dump the seeds and store on S3 so other nodes can use that
+    # TODO: only enable this when production can take on the role as primary (and legacy harvester will be ignored)
+    if role == "primary" and False:
+        call_command("dump_resource", "edurep.EdurepOAIPMH")
+        ctx = Context(environment)
+        harvester_data_bucket = "s3://edushare-data/datasets/harvester/edurep"
+        ctx.run(f"aws s3 sync {settings.DATAGROWTH_DATA_DIR}/edurep {harvester_data_bucket}", echo=True)
