8b0f772c3f620199d7db4e866e10285b24cc1e2f
Fako Berkers <email@fakoberkers.nl>
2020-09-18 11:54:03 +0200

    Adds a way to start a secondary harvest to prevent Edurep calls

--- harvester/harvester/management/commands/run_harvest.py
+++ harvester/harvester/management/commands/run_harvest.py
@@ -13,8 +13,11 @@ class Command(HarvesterCommand):
         super().add_arguments(parser)
         parser.add_argument('-r', '--reset', action="store_true",
                             help="Resets the Dataset model to be empty and deletes all OAI-PMH data")
+        parser.add_argument('-s', '--secondary', action="store_true",
+                            help="Indicates to run the harvest on a secondary (replication) node")
 
     def handle(self, **options):
         reset = options["reset"]
+        role = "secondary" if options["secondary"] else "primary"
         self.info(f"Calling harvest outside of schedule; reset={reset}")
-        harvest(reset=reset)
+        harvest(role=role, reset=reset)
