de5dcde8dea92a4e051cbfd3ae2486f484f32600
Fako Berkers <email@fakoberkers.nl>
2020-09-23 13:50:51 +0200

    Revert "Fixes PEP8 warnings"
    This reverts commit ed62daa6681678c6bd67e43fd47ded1fcedbeed2.

--- harvester/core/management/commands/update_dataset.py
+++ harvester/core/management/commands/update_dataset.py
@@ -1,5 +1,7 @@
 from collections import defaultdict
+from zipfile import BadZipFile
 
+from django.core.exceptions import ValidationError
 from django.db.models import Count
 
 from datagrowth.utils import ibatch
@@ -8,6 +10,7 @@ from core.constants import HarvestStages
 from core.management.base import OutputCommand
 from core.utils.resources import get_material_resources
 from edurep.utils import get_edurep_oaipmh_seeds
+from core.models import CommonCartridge
 
 
 class Command(OutputCommand):
