84705d142070c361a9e82fd82074ca7dc551abea
Fako Berkers <email@fakoberkers.nl>
2020-10-07 15:43:31 +0200

    Adds no progress to all commands to prevent swamping of logs

--- harvester/harvester/background.py
+++ harvester/harvester/background.py
@@ -37,19 +37,19 @@ def harvest(role="primary", reset=False):
             dataset.reset()
         # First we call the commands that will query the OAI-PMH interfaces
         if role == "primary":
-            call_command("harvest_edurep_seeds", f"--dataset={dataset.name}")
+            call_command("harvest_edurep_seeds", f"--dataset={dataset.name}", "--no-progress")
         else:
-            call_command("load_edurep_oaipmh_data", "--force-download")
+            call_command("load_edurep_oaipmh_data", "--force-download", "--no-progress")
         # After getting all the metadata we'll download content
-        call_command("harvest_basic_content", f"--dataset={dataset.name}")
+        call_command("harvest_basic_content", f"--dataset={dataset.name}", "--no-progress")
         # We skip any video downloading/processing for now
         # Later we want YoutubeDL to download the videos and Amber to process them
         OAIPMHHarvest.objects.filter(stage=HarvestStages.BASIC).update(stage=HarvestStages.VIDEO)
         # Aggregating the metadata and content into the dataset
-        call_command("update_dataset", f"--dataset={dataset.name}")
+        call_command("update_dataset", f"--dataset={dataset.name}", "--no-progress")
         # Based on the dataset we push to Elastic Search
         extra_push_index_args = ["--recreate"] if reset else []
-        call_command("push_es_index", f"--dataset={dataset.name}", *extra_push_index_args)
+        call_command("push_es_index", f"--dataset={dataset.name}", "--no-progress", *extra_push_index_args)
 
     # After processing all active datasets we dump the seeds and store on S3 so other nodes can use that
     # TODO: only enable this when production can take on the role as primary (and legacy harvester will be ignored)
