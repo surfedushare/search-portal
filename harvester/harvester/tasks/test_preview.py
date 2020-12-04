733d186c8e003cd867a94c013ca7984060a7b1b7
Ruben-Hartog <rhartog@zilverline.com>
2020-12-04 15:43:57 +0100

    Generate thumbnails and cleanup filesystem

--- harvester/harvester/tasks/test_preview.py
+++ harvester/harvester/tasks/test_preview.py
@@ -1,5 +1,6 @@
-from unittest.mock import patch, Mock
+from unittest.mock import patch, Mock, call
 from django.test import TestCase, override_settings
+from PIL import Image
 
 from harvester.tasks.preview import generate_browser_preview
 from core.tests.factories import DocumentFactory
@@ -25,18 +26,33 @@ class PreviewTestCase(TestCase):
     @patch('core.models.ChromeScreenshotResource.success', return_value=True)
     @patch('core.models.ChromeScreenshotResource.close')
     @patch('boto3.client')
-    def test_uploaded_to_s3(self, s3_client, close_mock, screenshot_resource_success, screenshot_resource_run):
+    @patch('PIL.Image.open')
+    @patch('os.remove')
+    def test_uploaded_to_s3(self, os_remove, image_open, s3_client, close_mock,
+                            screenshot_resource_success, screenshot_resource_run):
         upload_mock = s3_client.return_value.upload_file = Mock()
         document = DocumentFactory.create()
 
         generate_browser_preview(document.id)
 
         s3_client.assert_called_once_with("s3")
-        upload_mock.assert_called_once_with(
-            f"/home/search-portal/screenshot-{document.id}.png",
-            "preview-bucket",
-            f"previews/{document.id}/preview.png"
-        )
+        upload_mock.assert_has_calls([
+            call(
+                f"/home/search-portal/screenshot-{document.id}.png",
+                "preview-bucket",
+                f"previews/{document.id}/preview.png"
+            ),
+            call(
+                f"/home/search-portal/screenshot-{document.id}-400x300.png",
+                "preview-bucket",
+                f"previews/{document.id}/preview-400x300.png"
+            ),
+            call(
+                f"/home/search-portal/screenshot-{document.id}-200x150.png",
+                "preview-bucket",
+                f"previews/{document.id}/preview-200x150.png"
+            )
+        ])
 
     @override_settings(BASE_DIR="/home/search-portal")
     @override_settings(AWS_PREVIEWS_BUCKET_NAME=None)
@@ -44,7 +60,10 @@ class PreviewTestCase(TestCase):
     @patch('core.models.ChromeScreenshotResource.success', return_value=True)
     @patch('core.models.ChromeScreenshotResource.close')
     @patch('boto3.client')
-    def test_not_uploaded_to_s3(self, s3_client, close_mock, screenshot_resource_success, screenshot_resource_run):
+    @patch('PIL.Image.open')
+    @patch('os.remove')
+    def test_not_uploaded_to_s3(self, os_remove, image_open, s3_client, close_mock,
+                                screenshot_resource_success, screenshot_resource_run):
         document = DocumentFactory.create()
 
         generate_browser_preview(document.id)
@@ -56,11 +75,59 @@ class PreviewTestCase(TestCase):
     @patch('core.models.ChromeScreenshotResource.run')
     @patch('core.models.ChromeScreenshotResource.success', return_value=True)
     @patch('core.models.ChromeScreenshotResource.close')
-    def test_writes_preview_url_to_properties(self, close_mock, screenshot_resource_success, screenshot_resource_run):
+    @patch('PIL.Image.open')
+    @patch('os.remove')
+    def test_writes_preview_url_to_properties(self, os_remove, image_open, close_mock,
+                                              screenshot_resource_success, screenshot_resource_run):
         document = DocumentFactory.create()
 
         generate_browser_preview(document.id)
 
         document.refresh_from_db()
 
-        self.assertEqual(document.properties["preview_path"], f"previews/{document.id}/preview.png")
+        self.assertEqual(document.properties["preview_path"], f"previews/{document.id}")
+
+    @override_settings(BASE_DIR="/home/search-portal")
+    @override_settings(AWS_PREVIEWS_BUCKET_NAME=None)
+    @patch('core.models.ChromeScreenshotResource.run')
+    @patch('core.models.ChromeScreenshotResource.success', return_value=True)
+    @patch('core.models.ChromeScreenshotResource.close')
+    @patch('PIL.Image.open')
+    @patch('os.remove')
+    def test_generates_thumbnails(self, os_remove, image_open, close_mock,
+                                  screenshot_resource_success, screenshot_resource_run):
+        resize_mock = Mock()
+        image_open.return_value.resize = resize_mock
+
+        document = DocumentFactory.create()
+
+        generate_browser_preview(document.id)
+
+        resize_mock.assert_has_calls([
+            call((400, 300), Image.ANTIALIAS),
+            call().save(f"/home/search-portal/screenshot-{document.id}-400x300.png"),
+            call((200, 150), Image.ANTIALIAS),
+            call().save(f"/home/search-portal/screenshot-{document.id}-200x150.png")
+        ])
+
+    @override_settings(BASE_DIR="/home/search-portal")
+    @override_settings(AWS_PREVIEWS_BUCKET_NAME=None)
+    @patch('core.models.ChromeScreenshotResource.run')
+    @patch('core.models.ChromeScreenshotResource.success', return_value=True)
+    @patch('core.models.ChromeScreenshotResource.close')
+    @patch('PIL.Image.open')
+    @patch('os.remove')
+    def test_cleanup(self, os_remove, image_open, close_mock,
+                     screenshot_resource_success, screenshot_resource_run):
+        resize_mock = Mock()
+        image_open.return_value.resize = resize_mock
+
+        document = DocumentFactory.create()
+
+        generate_browser_preview(document.id)
+
+        os_remove.assert_has_calls([
+            call(f"/home/search-portal/screenshot-{document.id}.png"),
+            call(f"/home/search-portal/screenshot-{document.id}-400x300.png"),
+            call(f"/home/search-portal/screenshot-{document.id}-200x150.png")
+        ])
