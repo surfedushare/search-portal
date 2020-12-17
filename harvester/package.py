8a871e77f5425eff87a28e7ac50bf10de7548839
Fako Berkers <email@fakoberkers.nl>
2020-12-17 16:57:34 +0100

    One more version bump

--- harvester/package.py
+++ harvester/package.py
@@ -1,5 +1,5 @@
 PACKAGE = {
-    "version": "1.17.17",
+    "version": "1.17.18",
     "name": "harvester",
     "cpu": "2048",
     "memory": "8192",
--- service/package.py
+++ service/package.py
@@ -1,5 +1,5 @@
 PACKAGE = {
-    "version": "1.17.17",
+    "version": "1.17.18",
     "name": "search-portal",
     "cpu": "1024",
     "memory": "2048"
