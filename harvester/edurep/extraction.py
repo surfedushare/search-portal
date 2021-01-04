d08d5ac9b1ec235e49de348c07c3628b23f2529f
Fako Berkers <email@fakoberkers.nl>
2021-01-04 15:45:03 +0100

    Extracts relationship information from NL-LOM

--- harvester/edurep/extraction.py
+++ harvester/edurep/extraction.py
@@ -224,3 +224,32 @@ class EdurepDataExtraction(object):
     def get_analysis_allowed(cls, soup, el):
         value = EdurepDataExtraction.get_copyright(soup, el)
         return (value is not None and "nd" not in value) and value != "yes"
+
+    @classmethod
+    def get_is_part_of(cls, soup, el):
+        is_part_of = el.find(string='ispartof')
+        if not is_part_of:
+            return
+        relation = is_part_of.find_parent('czp:relation')
+        if not relation:
+            return
+        catalog = relation.find(string='oai:surfsharekit.nl')
+        if not catalog:
+            return
+        catalog_entry = catalog.find_next('czp:entry')
+        return "surfsharekit:" + catalog_entry.text.strip()  # prefix excluded by Edurep, but it's needed
+
+    @classmethod
+    def get_has_part(cls, soup, el):
+        elements = el.find_all(string='haspart')
+        results = []
+        for element in elements:
+            relation = element.find_parent('czp:relation')
+            if not relation:
+                continue
+            catalog = relation.find(string='oai:surfsharekit.nl')
+            if not catalog:
+                continue
+            catalog_entry = catalog.find_next('czp:entry')
+            results.append("surfsharekit:" + catalog_entry.text.strip())  # prefixes excluded by Edurep, but are needed
+        return results
