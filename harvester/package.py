21d2a366065c0f6d9733bdd6bff8325336877723
Fako Berkers <email@fakoberkers.nl>
2021-01-13 15:05:37 +0100

    Changes domain name to edusources.nl

--- harvester/package.py
+++ harvester/package.py
@@ -1,5 +1,5 @@
 PACKAGE = {
-    "version": "1.17.31",
+    "version": "1.17.32",
     "name": "harvester",
     "cpu": "2048",
     "memory": "8192",
--- service/package.py
+++ service/package.py
@@ -1,5 +1,5 @@
 PACKAGE = {
-    "version": "1.17.31",
+    "version": "1.17.32",
     "name": "search-portal",
     "cpu": "1024",
     "memory": "2048"
