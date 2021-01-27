a2735d6f0bea380cb930f4c7a52ad25580009c94
Fako Berkers <email@fakoberkers.nl>
2021-01-27 11:05:27 +0100

    Refactors the e2e base classes to also use them in vendor tests

--- service/e2e_tests/base.py
+++ service/e2e_tests/base.py
@@ -2,30 +2,12 @@ from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium.webdriver.chrome.webdriver import WebDriver
 from selenium.webdriver.chrome.options import Options
 from django.conf import settings
-from django.test import override_settings
+from django.test import override_settings, TestCase
 
 from elasticsearch import Elasticsearch
 
 
-class BaseTestCase(StaticLiveServerTestCase):
-    fixtures = ['locales', 'privacy_statements']
-
-    def setUp(cls):
-        super().setUp()
-        chrome_options = Options()
-        chrome_options.add_argument("--headless")
-        chrome_options.add_argument("window-size=1920,1080")
-
-        cls.selenium = WebDriver(options=chrome_options)
-        cls.selenium.implicitly_wait(10)
-
-    def tearDown(cls):
-        super().tearDown()
-        cls.selenium.quit()
-
-
-@override_settings(ELASTICSEARCH_NL_INDEX="test-nl", ELASTICSEARCH_EN_INDEX="test-en")
-class ElasticSearchTestCase(BaseTestCase):
+class BaseElasticSearchMixin(object):
 
     @classmethod
     def index_body(cls, language):
@@ -139,3 +121,28 @@ class ElasticSearchTestCase(BaseTestCase):
     def tearDownClass(cls):
         cls.elastic.indices.delete(settings.ELASTICSEARCH_NL_INDEX)
         cls.elastic.indices.delete(settings.ELASTICSEARCH_EN_INDEX)
+
+
+@override_settings(ELASTICSEARCH_NL_INDEX="test-nl", ELASTICSEARCH_EN_INDEX="test-en")
+class BaseLiveServerTestCase(BaseElasticSearchMixin, StaticLiveServerTestCase):
+
+    fixtures = ['locales', 'filter-categories', 'privacy_statements']
+
+    def setUp(self):
+        super().setUp()
+        chrome_options = Options()
+        chrome_options.add_argument("--headless")
+        chrome_options.add_argument("window-size=1920,1080")
+
+        self.selenium = WebDriver(options=chrome_options)
+        self.selenium.implicitly_wait(10)
+
+    def tearDown(self):
+        super().tearDown()
+        self.selenium.quit()
+
+
+@override_settings(ELASTICSEARCH_NL_INDEX="test-nl", ELASTICSEARCH_EN_INDEX="test-en")
+class BaseElasticSearchTestCase(BaseElasticSearchMixin, TestCase):
+
+    fixtures = ['locales', 'filter-categories', 'privacy_statements']
