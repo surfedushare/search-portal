d067ddeb2bb6d59159ae3f016cf58c9d80f9cf95
Fako Berkers <email@fakoberkers.nl>
2021-03-02 10:01:18 +0100

    Ups version for release

--- harvester/package.py
+++ harvester/package.py
@@ -1,5 +1,5 @@
 PACKAGE = {
-    "version": "1.19.2",
+    "version": "1.19.3",
     "name": "harvester",
     "cpu": "2048",
     "memory": "8192",
--- service/package.py
+++ service/package.py
@@ -1,5 +1,5 @@
 PACKAGE = {
-    "version": "1.19.2",
+    "version": "1.19.3",
     "name": "search-portal",
     "cpu": "1024",
     "memory": "2048"
