ed623b7f2786fd9d96d6e80f94ac89e02026e9ee
Fako Berkers <email@fakoberkers.nl>
2021-05-05 10:09:17 +0200

    Makes clean_data available as task under its function name

--- harvester/core/tasks/commands.py
+++ harvester/core/tasks/commands.py
@@ -2,6 +2,6 @@ from django.core.management import call_command
 from celery import current_app as app
 
 
-@app.task()
+@app.task(name="clean_data")
 def clean_data():
     call_command("clean_data")
