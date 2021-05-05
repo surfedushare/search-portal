ab7dbeef98e98d8ed611b70db97115f5fc21a143
Fako Berkers <email@fakoberkers.nl>
2021-05-05 15:57:00 +0200

    Fixes migration conflict

--- harvester/core/migrations/0009_sharekit_speedup.py
+++ harvester/core/migrations/0009_sharekit_speedup.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('core', '0007_harvest_refactor_data'),
+        ('core', '0008_cleanup_command'),
     ]
 
     operations = [
