f5d4f65376724778b7f95048f12198ac312b8597
Fako Berkers <email@fakoberkers.nl>
2021-06-12 15:45:31 +0200

    Ups version to remove Deepl secrets

--- harvester/package.py
+++ harvester/package.py
@@ -1,5 +1,5 @@
 PACKAGE = {
-    "version": "1.21.3",
+    "version": "1.21.5",
     "name": "harvester",
     "cpu": "2048",
     "memory": "8192",
--- service/package.py
+++ service/package.py
@@ -1,5 +1,5 @@
 PACKAGE = {
-    "version": "1.21.3",
+    "version": "1.21.5",
     "name": "search-portal",
     "cpu": "1024",
     "memory": "2048"
