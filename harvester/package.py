e3caf11825fda432c116c2f8d6f3cf9de43450f0
Fako Berkers <email@fakoberkers.nl>
2021-07-28 16:12:16 +0200

    Ups version for testing

--- harvester/package.py
+++ harvester/package.py
@@ -1,5 +1,5 @@
 PACKAGE = {
-    "version": "1.23.2",
+    "version": "1.23.3",
     "name": "harvester",
     "cpu": "2048",
     "memory": "8192",
--- service/package.py
+++ service/package.py
@@ -1,5 +1,5 @@
 PACKAGE = {
-    "version": "1.23.2",
+    "version": "1.23.3",
     "name": "search-portal",
     "cpu": "1024",
     "memory": "2048"
