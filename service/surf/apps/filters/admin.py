5e5d72188f4fae61ebba58760ee5e12996d9b47d
Fako Berkers <email@fakoberkers.nl>
2021-08-01 16:41:09 +0200

    Adds filter action to unhide batches

--- service/surf/apps/filters/admin.py
+++ service/surf/apps/filters/admin.py
@@ -8,6 +8,18 @@ from mptt.admin import DraggableMPTTAdmin
 from surf.apps.filters import models
 
 
+def unhide_filters(modeladmin, request, queryset):
+    for obj in queryset:
+        for descendent in obj.get_descendants():
+            descendent.is_hidden = False
+            descendent.save()
+        obj.is_hidden = False
+        obj.save()
+
+
+unhide_filters.short_description = "Unhide all selected filters and its descendents"
+
+
 @admin.register(models.MpttFilterItem)
 class MpttFilterItemAdmin(DraggableMPTTAdmin):
 
@@ -16,3 +28,5 @@ class MpttFilterItemAdmin(DraggableMPTTAdmin):
     list_display = ('tree_actions', 'indented_title', 'is_hidden', 'is_manual',)
     list_display_links = ('indented_title',)
     list_filter = ('is_hidden',)
+
+    actions = [unhide_filters]
