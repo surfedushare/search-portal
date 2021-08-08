061bf87270dad85d3bf33068bab2cc23916e8bb9
Fako Berkers <email@fakoberkers.nl>
2021-08-08 10:05:12 +0200

    Allows longer extension id

--- harvester/core/models/datatypes/extension.py
+++ harvester/core/models/datatypes/extension.py
@@ -5,7 +5,7 @@ from datagrowth.datatypes import DocumentBase
 
 class Extension(DocumentBase):
 
-    id = models.CharField(primary_key=True, max_length=50)
+    id = models.CharField(primary_key=True, max_length=100)
     dataset_version = models.ForeignKey("DatasetVersion", blank=True, null=True, on_delete=models.CASCADE)
     # NB: Collection foreign key is added by the base class
     is_parent = models.BooleanField(default=False)
