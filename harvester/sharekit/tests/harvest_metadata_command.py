dfec7cea77754b83fd522da734929cd2fc77b50a
Fako Berkers <email@fakoberkers.nl>
2021-08-11 11:08:53 +0200

    Makes Sharekit the primary test target

--- harvester/sharekit/tests/harvest_metadata_command.py
+++ harvester/sharekit/tests/harvest_metadata_command.py
@@ -1,6 +1,5 @@
 from core.tests.commands.harvest_metadata import TestMetadataHarvest, TestMetadataHarvestWithHistory
 from core.constants import Repositories
-from core.models import HarvestSource, Collection
 from sharekit.tests.factories import SharekitMetadataHarvestFactory
 
 
@@ -13,16 +12,6 @@ class TestMetadataHarvestSharekit(TestMetadataHarvest):
     def setUpClass(cls):
         super().setUpClass()
         SharekitMetadataHarvestFactory.create_common_sharekit_responses()
-        sharekit = HarvestSource.objects.get(id=1)
-        sharekit.repository = cls.repository
-        sharekit.spec = cls.spec_set
-        sharekit.save()
-        other = HarvestSource.objects.get(id=2)
-        other.repository = cls.repository
-        other.save()
-
-    def setUp(self):
-        super().setUp()
 
 
 class TestMetadataHarvestWithHistorySharekit(TestMetadataHarvestWithHistory):
@@ -34,19 +23,3 @@ class TestMetadataHarvestWithHistorySharekit(TestMetadataHarvestWithHistory):
     def setUpClass(cls):
         super().setUpClass()
         SharekitMetadataHarvestFactory.create_common_sharekit_responses(include_delta=True)
-        sharekit = HarvestSource.objects.get(id=1)
-        sharekit.repository = cls.repository
-        sharekit.spec = cls.spec_set
-        sharekit.save()
-        other = HarvestSource.objects.get(id=2)
-        other.repository = cls.repository
-        other.save()
-        collection = Collection.objects.get(id=171)
-        collection.name = cls.spec_set
-        collection.save()
-        collection.documents \
-            .filter(properties__title="Using a Vortex | Wageningen UR") \
-            .update(reference="5be6dfeb-b9ad-41a8-b4f5-94b9438e4257")
-
-    def setUp(self):
-        super().setUp()
