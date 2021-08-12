787dac98c8bfa1df854d2f2d7c294627a981c79c
Fako Berkers <email@fakoberkers.nl>
2021-08-12 09:56:07 +0200

    Ups version for fix

--- harvester/package.py
+++ harvester/package.py
@@ -1,5 +1,5 @@
 PACKAGE = {
-    "version": "1.23.9",
+    "version": "1.23.10",
     "name": "harvester",
     "cpu": "2048",
     "memory": "8192",
--- service/package.py
+++ service/package.py
@@ -1,5 +1,5 @@
 PACKAGE = {
-    "version": "1.23.9",
+    "version": "1.23.10",
     "name": "search-portal",
     "cpu": "1024",
     "memory": "2048"
