1024a3a8a50a7454df1d1fab150cf378f58d75f9
Fako Berkers <email@fakoberkers.nl>
2021-08-13 14:58:12 +0200

    Adds some basic authentication to the harvester API

--- harvester/harvester/settings.py
+++ harvester/harvester/settings.py
@@ -189,6 +189,12 @@ else:
 
 REST_FRAMEWORK = {
     'DEFAULT_VERSIONING_CLASS': 'rest_framework.versioning.NamespaceVersioning',
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication'
+    ]
 }
 
 
