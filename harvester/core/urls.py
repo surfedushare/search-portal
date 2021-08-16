7ecaf1d9c16d6d31460bbfc20e8313b3045bec34
Fako Berkers <email@fakoberkers.nl>
2021-08-16 18:48:07 +0200

    Improves documentation for extensions

--- harvester/core/urls.py
+++ harvester/core/urls.py
@@ -8,6 +8,6 @@ urlpatterns = public_api_patterns = [
     path('dataset/<int:pk>/documents/', views.DatasetDocumentsView.as_view(), name="dataset-documents"),
     path('dataset/<int:pk>/', views.DatasetDetailView.as_view(), name="dataset-detail"),
     path('dataset/', views.DatasetListView.as_view(), name="datasets"),
-    path('extension/<str:pk>/', views.ExtensionDetailView.as_view(), name="extension-detail"),
+    path('extension/<str:external_id>/', views.ExtensionDetailView.as_view(), name="extension-detail"),
     path('extension/', views.ExtensionListView.as_view(), name="extensions"),
 ]
--- harvester/core/views/extension.py
+++ harvester/core/views/extension.py
@@ -4,17 +4,38 @@ from rest_framework import serializers
 from rest_framework.exceptions import ValidationError
 
 from datagrowth.datatypes.views import DocumentBaseSerializer
+from harvester.schema import HarvesterSchema
 from core.models import Extension, Document
 
 from project.serializers import PersonSerializer, OrganisationSerializer, ProjectSerializer, LabelSerializer
 
 
-class ExtensionSerializer(DocumentBaseSerializer):
+class ExtensionPropertiesSerializer(serializers.Serializer):
+
+    external_id = serializers.CharField()
+    title = serializers.CharField(required=False)
+    description = serializers.CharField(required=False)
+    language = serializers.CharField(required=False, max_length=2)
+    published_at = serializers.DateField(required=False)
+    copyright = serializers.ChoiceField(required=False, choices=settings.COPYRIGHT_VALUES)
+
+    authors = PersonSerializer(many=True, required=False)
+    parties = OrganisationSerializer(many=True, required=False)
+    projects = ProjectSerializer(many=True, required=False)
+    themes = LabelSerializer(many=True, required=False)
+    keywords = LabelSerializer(many=True, required=False)
+
+    parents = serializers.ListField(child=serializers.CharField(), required=False)
+    children = serializers.ListField(child=serializers.CharField(), required=False)
+
+
+class ExtensionSerializer(DocumentBaseSerializer, ExtensionPropertiesSerializer):
 
     id = serializers.CharField(read_only=True)
-    external_id = serializers.CharField(write_only=True)
     is_parent = serializers.BooleanField(required=False, default=False)
+    properties = ExtensionPropertiesSerializer(read_only=True)
 
+    external_id = serializers.CharField(write_only=True)
     title = serializers.CharField(write_only=True, required=False)
     description = serializers.CharField(write_only=True, required=False)
     language = serializers.CharField(write_only=True, required=False, max_length=2)
@@ -31,7 +52,7 @@ class ExtensionSerializer(DocumentBaseSerializer):
     children = serializers.ListField(child=serializers.CharField(), write_only=True, required=False)
 
     def validate_external_id(self, external_id):
-        path_external_id = self.context["view"].kwargs.get("pk")
+        path_external_id = self.context["view"].kwargs.get("external_id")
         if path_external_id and path_external_id != external_id:
             raise ValidationError("External id in path and body do not match.")
         return external_id
@@ -105,10 +126,112 @@ class ExtensionSerializer(DocumentBaseSerializer):
 
 
 class ExtensionListView(generics.ListCreateAPIView):
+    """
+    Returns a list of all existing extensions (GET) and allows for adding new extensions (POST).
+
+    An extension is a way for anybody to enrich the data that is coming from repositories.
+    When documents from repositories get indexed by the search engine
+    the properties of an extension will overwrite the values from repository documents,
+    except for the "children" and "parents" properties.
+    Those lists get concatenated with the values from repository documents.
+
+    ## Request body
+
+    To create an extension it should get posted to this endpoint.
+    Below is a list of properties that you can set on an extension.
+    When performing a GET the same properties can be read.
+
+    **external_id**: The identifier of the document that this extension extends.
+    These identifiers are provided by the repository that provided the metadata.
+    When is_parent is true you must provide your own unique identifier.
+
+    **is_parent**: Sometimes an extension only acts as a parent to bind certain documents together.
+    is_parent should be true if you want to create such an extension.
+    Since there exists no repository document for this type of extension,
+    the external_id must be set by its creator upon a POST.
+    The values of the title, description, language, published_at and copyright properties (if given)
+    will be used as data to search through instead of the values from an underlying document.
+
+    **authors**: (optional) The list of authors that this extension should overwrite.
+    An author consists of a name and email address.
+
+    **parties**: (optional) A list of organizations or agents that this extension should add to the search data.
+    A party consists of a name.
+
+    **projects**: (optional) A list of projects that this extension should adds to the search data.
+    A project consists of a name.
+
+    **themes**: (optional) A list of themes that this extension should overwrite.
+    A theme Consists of a label.
+
+    **keywords**: (optional) A list of keywords that this extension should overwrite.
+    A keyword consists of a label.
+
+    **parents**: (optional) A list of external_id identifiers that will be added as parents by the extension.
+
+    **childs**: (optional) A list of external_id identifiers that will be added as children by the extension.
+
+    ## Response body
+
+    The response contains a list of extensions (GET) or the newly created extension (POST).
+    See request body to learn more about the properties of an extension.
+    """
     queryset = Extension.objects.all()
     serializer_class = ExtensionSerializer
+    schema = HarvesterSchema()
 
 
 class ExtensionDetailView(generics.RetrieveUpdateDestroyAPIView):
+    """
+    This endpoint allows you to retrieve, update or delete an extension.
+
+    An extension is a way for anybody to enrich the data that is coming from repositories.
+    When documents from repositories get indexed by the search engine
+    the properties of an extension will overwrite the values from repository documents,
+    except for the "children" and "parents" properties.
+    Those lists get concatenated with the values from repository documents.
+
+    ## Request body
+
+    To update an extension you should PUT it to this endpoint.
+    Below is a list of properties that you can set on an extension.
+    When performing a GET the same properties can be read.
+
+    **external_id**: The identifier of the document that this extension extends.
+    These identifiers are provided by the repository that provided the metadata or when is_parent is true
+    by the creator of the extension.
+
+    **is_parent**: Sometimes an extension only acts as a parent to bind certain documents together.
+    is_parent will be true for such an extension. It is not possible to alter the is_parent property after creation.
+    The values of the title, description, language, published_at and copyright properties (if given)
+    will be used as data to search through instead of the values from an underlying document.
+    You can update these properties.
+
+    **authors**: (optional) The list of authors that this extension should overwrite.
+    An author consists of a name and email address.
+
+    **parties**: (optional) A list of organizations or agents that this extension should add to the search data.
+    A party consists of a name.
+
+    **projects**: (optional) A list of projects that this extension should adds to the search data.
+    A project consists of a name.
+
+    **themes**: (optional) A list of themes that this extension should overwrite.
+    A theme Consists of a label.
+
+    **keywords**: (optional) A list of keywords that this extension should overwrite.
+    A keyword consists of a label.
+
+    **parents**: (optional) A list of external_id identifiers that will be added as parents by the extension.
+
+    **childs**: (optional) A list of external_id identifiers that will be added as children by the extension.
+
+    ## Response body
+
+    The response contains an extension (GET or PUT) or an empty response (DELETE).
+    See request body to learn more about the properties of an extension.
+    """
     queryset = Extension.objects.all()
     serializer_class = ExtensionSerializer
+    lookup_url_kwarg = "external_id"
+    schema = HarvesterSchema()
