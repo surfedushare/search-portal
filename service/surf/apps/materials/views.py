11ae1a71c5e92bb69e7886d5e7d691345f5a8841
Fako Berkers <email@fakoberkers.nl>
2021-11-04 17:24:18 +0100

    Filter most recent on AnatomyTOOL

--- service/surf/apps/materials/views.py
+++ service/surf/apps/materials/views.py
@@ -287,6 +287,10 @@ class MaterialAPIView(APIView):
             res = elastic.search('',
                                  # sort by newest items first
                                  ordering="-lom.lifecycle.contribute.publisherdate",
+                                 filters=[{
+                                     "external_id": "harvest_source",
+                                     "items": ["anatomy_tool"]
+                                 }],
                                  page_size=_MATERIALS_COUNT_IN_OVERVIEW)
 
             res = add_extra_parameters_to_materials(request.user,
