4c275f0cabd42fddef8d37cba26ceb87ffae1f6c
Fako Berkers <email@fakoberkers.nl>
2021-11-25 12:31:27 +0100

    Fixes migration conflict

--- harvester/core/migrations/0028_rename_is_addition.py
+++ harvester/core/migrations/0028_rename_is_addition.py
@@ -6,7 +6,7 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('core', '0026_alter_objectiveproperty_property'),
+        ('core', '0027_query_annotations'),
     ]
 
     operations = [
