5e650e9e6d7667d1719db33917bb451f9707d437
Fako Berkers <email@fakoberkers.nl>
2022-01-03 17:41:18 +0100

    Caches the metadata property under a ttl

--- service/surf/apps/filters/apps.py
+++ service/surf/apps/filters/apps.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.apps import AppConfig
+from django.core.cache import cache
 
 from surf.apps.filters.metadata import MetadataTree
 
@@ -7,7 +8,16 @@ from surf.apps.filters.metadata import MetadataTree
 class FiltersConfig(AppConfig):
 
     name = 'surf.apps.filters'
-    metadata = MetadataTree(
-        harvester_url=settings.HARVESTER_API,
-        api_token=settings.HARVESTER_API_KEY
-    )
+
+    @property
+    def metadata(self):
+        cache_key = "filters.metadata"
+        metadata = cache.get(cache_key, None)
+        if metadata is not None:
+            return metadata
+        metadata = MetadataTree(
+            harvester_url=settings.HARVESTER_API,
+            api_token=settings.HARVESTER_API_KEY
+        )
+        cache.set(cache_key, metadata, 60*60)
+        return metadata
