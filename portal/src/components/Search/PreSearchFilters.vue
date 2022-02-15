033e32ce2dcf7a02f3e8904b542f562bdcd1e3f5
Naftali van der Loon <naftali@xsenzis.nl>
2022-02-15 11:05:50 +0100

    makes sure that data is there before rendering

--- portal/src/components/Search/PreSearchFilters.vue
+++ portal/src/components/Search/PreSearchFilters.vue
@@ -1,5 +1,5 @@
 <template>
-  <div tabindex="-1" @focusout="onFocusOut">
+  <div v-if="dropdownData" tabindex="-1" @focusout="onFocusOut">
     <FilterDropdown
       v-for="dropdown in dropdowns"
       :key="dropdown.field"
@@ -7,7 +7,7 @@
       :field="dropdown.field"
       :default-option="dropdown.defaultOption"
       :visible="dropdown.visible"
-      :filters="dropdownData ? dropdownData[dropdown.field].children : []"
+      :filters="dropdownData && dropdownData[dropdown.field] ? dropdownData[dropdown.field].children : []"
       @toggle="onToggle"
       @update:selection="onSelection"
     />
@@ -84,7 +84,7 @@ export default {
     },
     onFocusOut(event) {
       const element = event.target;
-      if (!element.contains(event.relatedTarget) && event.relatedTarget?.className !== 'main__info_search__domain' && event.relatedTarget?.type !== 'checkbox') {
+      if (!element.contains(event.relatedTarget) && event.relatedTarget?.className !== 'search_bar__filters' && event.relatedTarget?.type !== 'checkbox') {
         this.dropdowns.map(dd => dd.visible = false);
       }
     }
