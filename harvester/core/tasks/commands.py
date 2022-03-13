cc50077921c9b864ddadce0ea97002f5e369596e
Fako Berkers <email@fakoberkers.nl>
2022-03-13 08:39:46 +0100

    Makes index switching possible through the admin

--- harvester/core/tasks/commands.py
+++ harvester/core/tasks/commands.py
@@ -5,3 +5,8 @@ from celery import current_app as app
 @app.task(name="clean_data")
 def clean_data():
     call_command("clean_data")
+
+
+@app.task(name="promote_dataset_version")
+def promote_dataset_version(dataset_version_id):
+    call_command("promote_dataset_version", f"--dataset-version-id={dataset_version_id}")
