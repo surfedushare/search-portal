84122ff9e7b8dbc3f4ced485f5360cb84514d41d
Fako Berkers <email@fakoberkers.nl>
2022-03-21 08:37:33 +0100

    Always clear harvest resources when preparing

--- harvester/core/utils/harvest.py
+++ harvester/core/utils/harvest.py
@@ -13,11 +13,11 @@ def prepare_harvest(dataset, reset=False):
 
     excluded_specs = []
     for harvest in Harvest.objects.filter(dataset=dataset).select_for_update():
+        logger.info(f"Clearing resources for '{harvest.source.name}'")
+        harvest.source.clear_repository_resources()
         if reset or harvest.should_purge():
             logger.debug(f"Resetting harvest stage for '{harvest.source.name}'")
             harvest.reset()
-            logger.info(f"Clearing resources for '{harvest.source.name}'")
-            harvest.source.clear_repository_resources()
             excluded_specs.append(harvest.source.spec)
         else:
             logger.debug(f"Setting harvest stage to NEW for '{harvest.source.name}'")
