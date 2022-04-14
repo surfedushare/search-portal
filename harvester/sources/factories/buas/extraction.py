207fc0628b688400ad7b5ee081c9ff9f2f059326
Fako Berkers <email@fakoberkers.nl>
2022-04-14 18:08:12 +0200

    Adds BUAS as a source

---
+++ harvester/sources/factories/buas/extraction.py
@@ -0,0 +1,64 @@
+import os
+import factory
+from datetime import datetime
+
+from django.conf import settings
+from django.utils.timezone import make_aware
+
+from sources.models import BuasPureResource
+
+
+SLUG = "buas"
+ENDPOINT = BuasPureResource.URI_TEMPLATE.replace("https://", "")
+SET_SPECIFICATION = "buas"
+
+
+class BuasPureResourceFactory(factory.django.DjangoModelFactory):
+
+    class Meta:
+        model = BuasPureResource
+        strategy = factory.BUILD_STRATEGY
+
+    class Params:
+        is_initial = True
+        number = 0
+
+    since = make_aware(datetime(year=1970, month=1, day=1))
+    set_specification = SET_SPECIFICATION
+    status = 200
+    head = {
+        "content-type": "application/json"
+    }
+
+    @factory.lazy_attribute
+    def uri(self):
+        offset = self.number * 10
+        return f"{ENDPOINT}?offset={offset}&size=10"
+
+    @factory.lazy_attribute
+    def request(self):
+        return {
+            "args": [f"{self.since:%Y-%m-%dT%H:%M:%SZ}"],
+            "kwargs": {},
+            "method": "get",
+            "url": "https://" + self.uri,
+            "headers": {},
+            "data": {}
+        }
+
+    @factory.lazy_attribute
+    def body(self):
+        response_type = "initial"
+        response_file = f"fixture.{SLUG}.{response_type}.{self.number}.json"
+        response_file_path = os.path.join(
+            settings.BASE_DIR, "sources", "factories",
+            SLUG,
+            response_file
+        )
+        with open(response_file_path, "r") as response:
+            return response.read()
+
+    @classmethod
+    def create_common_responses(cls):
+        cls.create(number=0)
+        cls.create(number=1)
