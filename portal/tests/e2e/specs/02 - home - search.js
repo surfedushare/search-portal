c800d1568d85d9236de47363997adf040e2ce8e7
Naftali van der Loon <naftali@xsenzis.nl>
2022-04-19 10:35:44 +0200

    Uses dataset in tests

--- portal/tests/e2e/specs/02 - home - search.js
+++ portal/tests/e2e/specs/02 - home - search.js
@@ -4,42 +4,42 @@ describe("Home page - search", () => {
   });
 
   it("Should find materials by search term", () => {
-    cy.searchFor("Wiskunde");
-    cy.searchResultsShouldContain("wiskunde");
+    cy.searchFor("How");
+    cy.searchResultsShouldContain("how", 7);
   });
 
   it("Should give search term suggestions and use the search suggestion as search term when clicked", () => {
     cy.get("[data-test=search_term]")
-      .type("muscul")
+      .type("lea")
       .get("[role=listbox]")
       .should("be.visible")
       .children()
-      .should("have.length", 6)
-      .eq(2)
-      .should("contain", "muscularis")
+      .should("have.length", 2)
+      .eq(1)
+      .should("contain", "learn")
       .click(50, 25, { force: true })
-      .searchResultsShouldContain("muscularis", 10, 1);
+      .searchResultsShouldContain("Learn", 2, 0);
   });
 
   it("Should find materials by author", () => {
-    cy.searchFor("A Borst");
-    cy.searchResultsShouldContain("A. Borst");
+    cy.searchFor("esther");
+    cy.searchResultsShouldContain("Esther Quaedackers", 5, 1);
   });
 
   it("Should show search suggestion when a search suggestion is available", () => {
-    cy.searchFor("soocial");
+    cy.searchFor("thee");
     cy.get("[data-test=search_suggestion]")
       .should("be.visible")
       .should(
         "contain",
-        "Bedoelde je social ? Want er zijn geen resultaten voor 'soocial'"
+        "Bedoelde je these ? Want er zijn geen resultaten voor 'thee'"
       );
   });
 
   it("Should search with search suggestion when the search suggestion is clicked", () => {
-    cy.searchFor("soocial");
+    cy.searchFor("thee");
     cy.get("[data-test=search_suggestion_link]").should("be.visible").click();
-    cy.searchResultsShouldContain("Social");
+    cy.searchResultsShouldContain("these", 3, 1);
   });
 
   it("Should show 'no search result' message when no search suggestion is available", () => {
