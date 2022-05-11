066a1b4f4cf7d57b221ef6de3d9ae2fea3d989f4
Fako Berkers <email@fakoberkers.nl>
2022-05-11 09:43:19 +0200

    Implements build job

--- commands/deploy/container.py
+++ commands/deploy/container.py
@@ -7,11 +7,12 @@ from invoke.exceptions import Exit
 from git import Repo
 
 from commands import TARGETS
-from environments.project import REPOSITORY, REPOSITORY_AWS_PROFILE
-from environments.utils.packaging import get_package_info
+from environments.project import REPOSITORY
 
 
-@task()
+@task(help={
+    "commit": "The commit hash a new build should include in its info.json"
+})
 def prepare_builds(ctx, commit=None):
     """
     Makes sure that repo information will be present inside Docker images
@@ -19,52 +20,43 @@ def prepare_builds(ctx, commit=None):
     if commit is None:
         repo = Repo(".")
         commit = str(repo.head.commit)
-    # TODO: we can make assertions about the git state like: no uncommited changes and no untracked files
-    with open(os.path.join("portal", "package.json")) as portal_package_file:
-        portal_package = json.load(portal_package_file)
+
     service_package = TARGETS["service"]
     harvester_package = TARGETS["harvester"]
     info = {
         "commit": commit,
         "versions": {
             "service": service_package["version"],
-            "harvester": harvester_package["version"],
-            "portal": portal_package["version"]
+            "harvester": harvester_package["version"]
         }
     }
     with open(os.path.join("environments", "info.json"), "w") as info_file:
         json.dump(info, info_file)
 
 
-@task(prepare_builds, help={
+@task(help={
     "target": "Name of the project you want to build: service or harvester",
-    "version": "Version of the project you want to build. Must match value in package.py"
+    "commit": "The commit hash a new build should include in its info.json. Will also be used to tag the new image."
 })
-def build(ctx, target, version):
+def build(ctx, target, commit=None):
     """
     Uses Docker to build an image for a Django project
     """
+    prepare_builds(ctx, commit)
 
     # Check the input for validity
     if target not in TARGETS:
         raise Exit(f"Unknown target: {target}", code=1)
-    package_info = get_package_info()
-    package_version = package_info["versions"][target]
-    if package_version != version:
-        raise Exit(
-            f"Expected version of {target} to match {version} instead it's {package_version}. Update package.py?",
-            code=1
-        )
 
     # Gather necessary info and call Docker to build
     target_info = TARGETS[target]
     ctx.run(
-        f"docker build -f {target}/Dockerfile -t {target_info['name']}:{version} .",
+        f"docker build -f {target}/Dockerfile -t {target_info['name']}:{commit} .",
         pty=True,
         echo=True
     )
     ctx.run(
-        f"docker build -f nginx/Dockerfile-nginx -t {target_info['name']}-nginx:{version} .",
+        f"docker build -f nginx/Dockerfile-nginx -t {target_info['name']}-nginx:{commit} .",
         pty=True,
         echo=True
     )
@@ -72,9 +64,9 @@ def build(ctx, target, version):
 
 @task(help={
     "target": "Name of the project you want to push to AWS registry: service or harvester",
-    "version": "Version of the project you want to push. Defaults to latest version"
+    "commit": "The commit hash that the image to be pushed is tagged with."
 })
-def push(ctx, target, version=None):
+def push(ctx, target, commit, docker_login=False):
     """
     Pushes a previously made Docker image to the AWS container registry, that's shared between environments
     """
@@ -84,7 +76,6 @@ def push(ctx, target, version=None):
         raise Exit(f"Unknown target: {target}", code=1)
     # Load info
     target_info = TARGETS[target]
-    version = version or target_info["version"]
     name = target_info["name"]
 
     # Login with Docker to AWS
@@ -93,12 +84,12 @@ def push(ctx, target, version=None):
         f"docker login --username AWS --password-stdin {REPOSITORY}",
         echo=True
     )
-    # Tag the main image and push
-    ctx.run(f"docker tag {name}:{version} {REPOSITORY}/{name}:{version}", echo=True)
-    ctx.run(f"docker push {REPOSITORY}/{name}:{version}", echo=True, pty=True)
-    # Tag Nginx and push
-    ctx.run(f"docker tag {name}-nginx:{version} {REPOSITORY}/{name}-nginx:{version}", echo=True)
-    ctx.run(f"docker push {REPOSITORY}/{name}-nginx:{version}", echo=True, pty=True)
+
+    # Tagging and pushing of our image and nginx image
+    ctx.run(f"docker tag {name}:{commit} {REPOSITORY}/{name}:{commit}", echo=True)
+    ctx.run(f"docker push {REPOSITORY}/{name}:{commit}", echo=True, pty=True)
+    ctx.run(f"docker tag {name}-nginx:{commit} {REPOSITORY}/{name}-nginx:{commit}", echo=True)
+    ctx.run(f"docker push {REPOSITORY}/{name}-nginx:{commit}", echo=True, pty=True)
 
 
 @task(help={
