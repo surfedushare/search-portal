333a7fe08ce30095fb67b6799ae9034b48041c54
Fako Berkers <email@fakoberkers.nl>
2022-06-08 18:09:38 +0200

    Tests with boost of 10

--- environments/edusources/configuration.py
+++ environments/edusources/configuration.py
@@ -1,7 +1,7 @@
 REPOSITORY = "017973353230.dkr.ecr.eu-central-1.amazonaws.com"
 REPOSITORY_AWS_PROFILE = "pol-prod"
 SEARCH_FIELDS = [
-    "title^5", "title.analyzed^5", "title.folded^5",
+    "title^10", "title.analyzed^10", "title.folded^10",
     "text", "text.analyzed", "text.folded",
     "description", "description.analyzed", "description.folded",
     "keywords", "keywords.folded",
