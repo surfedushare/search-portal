00d09558ab1f0479081ea819c7c73c08f2c16fed
Naftali van der Loon <naftali@xsenzis.nl>
2022-06-20 11:22:29 +0200

    Bump to 1.29.18

--- harvester/package.py
+++ harvester/package.py
@@ -1,5 +1,5 @@
 PACKAGE = {
-    "version": "1.29.17",
+    "version": "1.29.18",
     "name": "harvester",
     "cpu": "2048",
     "memory": "8192",
--- service/package.py
+++ service/package.py
@@ -1,5 +1,5 @@
 PACKAGE = {
-    "version": "1.29.17",
+    "version": "1.29.18",
     "name": "search-portal",
     "cpu": "1024",
     "memory": "2048"
