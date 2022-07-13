dd9058822fcfee61629313218b83a0cfb2db2252
Fako Berkers <email@fakoberkers.nl>
2022-07-13 08:32:42 +0200

    Prevents file overwrite for community images

--- service/surf/settings/base.py
+++ service/surf/settings/base.py
@@ -242,6 +242,7 @@ if MODE != 'localhost':
     DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
     AWS_STORAGE_BUCKET_NAME = environment.aws.image_upload_bucket
     AWS_S3_REGION_NAME = 'eu-central-1'
+    AWS_S3_FILE_OVERWRITE = False
 
 AWS_HARVESTER_BUCKET_NAME = environment.aws.harvest_content_bucket
 
