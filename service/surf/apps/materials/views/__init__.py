cec19e46d685c6de089e54bcd0e18a13083bd584
Fako Berkers <email@fakoberkers.nl>
2022-09-07 16:29:00 +0200

    Renames and logically splits materials views

---
+++ service/surf/apps/materials/views/__init__.py
@@ -0,0 +1,5 @@
+from surf.apps.materials.views.search import portal_material, portal_single_page_application, portal_page_not_found
+from surf.apps.materials.views.search import MaterialSearchAPIView, KeywordsAPIView, MaterialAPIView
+from surf.apps.materials.views.search import SimilarityAPIView, AuthorSuggestionsAPIView
+from surf.apps.materials.views.search import (MaterialRatingAPIView, MaterialApplaudAPIView,
+                                              CollectionMaterialPromotionAPIView, CollectionViewSet, MaterialSetAPIView)
