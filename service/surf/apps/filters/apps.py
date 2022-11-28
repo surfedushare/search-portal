daa14f4abf78b5b3a04e3a81dacf306ea06186c5
Fako Berkers <email@fakoberkers.nl>
2022-11-28 17:32:12 +0100

    Makes cache dependant on site slug to prevent clash on site id

--- service/surf/apps/filters/apps.py
+++ service/surf/apps/filters/apps.py
@@ -11,7 +11,7 @@ class FiltersConfig(AppConfig):
 
     @property
     def metadata(self):
-        cache_key = f"filters.metadata&site_id={settings.SITE_ID}"
+        cache_key = f"filters.metadata&site={settings.SITE_SLUG}"
         metadata = cache.get(cache_key, None)
         if metadata is not None:
             return metadata
