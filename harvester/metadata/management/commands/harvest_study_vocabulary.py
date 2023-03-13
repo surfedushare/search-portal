ec7ea064f90c5affc0aa298151cf1f26b21aed5b
Kalle Pronk <kallepronk@gmail.com>
2023-03-13 15:53:13 +0100

    Refactored harvest_study_vocabulary.py. Now possible to run specific vocabulary. Doesn't crash because of duplicates anymore

--- harvester/metadata/management/commands/harvest_study_vocabulary.py
+++ harvester/metadata/management/commands/harvest_study_vocabulary.py
@@ -5,13 +5,12 @@ from metadata.utils.translate import translate_with_deepl
 from metadata.models import StudyVocabularyResource, MetadataField, MetadataTranslation, MetadataValue
 from datagrowth.configuration import create_config
 from datagrowth.processors import ExtractProcessor
-import ipdb;
 
 logger = logging.getLogger("harvester")
 
 
 def create_metadata_value(term, field, parent):
-    if not MetadataValue.objects.filter(value=term["name"]).exists():
+    if not MetadataValue.objects.filter(value=str(term["value"])).exists():
         vocabulary = MetadataValue(value=term["value"])
         translation = MetadataTranslation.objects.create(
             nl=term["name"],
@@ -21,20 +20,26 @@ def create_metadata_value(term, field, parent):
         vocabulary.field = field
         vocabulary.name = str(term["name"])
         vocabulary.parent = parent
+        vocabulary.save()
         return vocabulary
     else:
-        return MetadataValue.objects.get(value=term["name"])
+        return MetadataValue.objects.get(value=str(term["value"]))
 
 
 class Command(BaseCommand):
+
+    def add_arguments(self, parser):
+        super().add_arguments(parser)
+        parser.add_argument('--vocabulary',
+                            choices=["applied-science", "informatievaardigheid", "vaktherapie", "verpleegkunde"])
 
     domain_dictionary = {
         "applied-science": {
             "path": "applied-science/applied-science-2021.skos.json",
             "nl": "Toegepaste Wetenschappen",
             "en": "Applied Science",
-            "value": "applied_science",
-            "name": "applied_science",
+            "value": "applied-science",
+            "name": "applied-science",
             "language": "nl"
         },
         "informatievaardigheid": {
@@ -65,6 +70,28 @@ class Command(BaseCommand):
 
     def handle(self, **options):
 
+        vocabulary = options["vocabulary"]
+
+        field_translation, _ = MetadataTranslation.objects.get_or_create(
+            nl="Vakvocabulaire",
+            en="Study vocabulary"
+        )
+        field, _ = MetadataField.objects.get_or_create(
+            name="Vakvocabulaire",
+            defaults={"translation": field_translation}
+        )
+
+        vocabulary_list = []
+        if vocabulary is not None:
+            self.export_vocabulary(key=vocabulary, field=field, output_list=vocabulary_list)
+        else:
+            for key in self.domain_dictionary:
+                self.export_vocabulary(key=key, field=field, output_list=vocabulary_list)
+
+        logger.info('Done with study vocabulary harvest')
+
+    def export_vocabulary(self, key, field, output_list):
+
         config = create_config("extract_processor", {
             "objective": {
                 "@": "$.@graph",
@@ -77,32 +104,18 @@ class Command(BaseCommand):
 
         extractor = ExtractProcessor(config=config)
 
-        field_translation, _ = MetadataTranslation.objects.get_or_create(
-            nl="Vakvocabulaire",
-            en="Study vocabulary"
-        )
-        field, _ = MetadataField.objects.get_or_create(
-            name="Vakvocabulaire",
-            defaults={"translation": field_translation}
-        )
-
-        vocabulary_list = []
-
-        for key in self.domain_dictionary:
-            raw_source = StudyVocabularyResource().get(self.domain_dictionary[key]["path"])
-            searched_source = extractor.extract(*raw_source.content)
-            vocab_frame = pd.DataFrame.from_records(searched_source).fillna("root")
-            vocab_groups = vocab_frame.groupby("parent_id").groups
-            root = create_metadata_value(field=field, term=self.domain_dictionary[key], parent=None)
-            root.save()
-            for sub_root in vocab_groups["root"].tolist():
-                self.depth_first_algorithm(value=sub_root, parent=root, field=field, groups=vocab_groups,
-                                           frame=vocab_frame, output_list=vocabulary_list)
-        logger.info('Done with study vocabulary harvest')
+        raw_source = StudyVocabularyResource().get(self.domain_dictionary[key]["path"])
+        raw_source.close()
+        searched_source = extractor.extract(*raw_source.content)
+        vocab_frame = pd.DataFrame.from_records(searched_source).fillna("root")
+        vocab_groups = vocab_frame.groupby("parent_id").groups
+        root = create_metadata_value(field=field, term=self.domain_dictionary[key], parent=None)
+        for sub_root in vocab_groups["root"].tolist():
+            self.depth_first_algorithm(value=sub_root, parent=root, field=field, groups=vocab_groups,
+                                       frame=vocab_frame, output_list=output_list)
 
     def depth_first_algorithm(self, value, parent, field, groups, frame, output_list):
         new_term = create_metadata_value(term=frame.iloc[value], field=field, parent=parent)
-        new_term.save()
         output_list.append(new_term)
         try:
             for sub_values in groups[frame.iloc[value].value].tolist():
