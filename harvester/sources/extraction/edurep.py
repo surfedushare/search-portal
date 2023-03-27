6bfb51818332d8ac40a247bf7b19594f21a50518
Kalle Pronk <kallepronk@gmail.com>
2023-03-27 16:12:10 +0200

    Added files, made migration

---
+++ harvester/sources/extraction/edurep.py
@@ -0,0 +1,229 @@
+import re
+from mimetypes import guess_type
+from hashlib import sha1
+
+from django.conf import settings
+
+from datagrowth.processors import ExtractProcessor
+
+
+class EdurepMetadataExtraction(ExtractProcessor):
+
+    youtube_regex = re.compile(r".*(youtube\.com|youtu\.be).*", re.IGNORECASE)
+
+    @classmethod
+    def get_record_state(cls, node):
+        return "active"
+
+    #############################
+    # GENERIC
+    #############################
+
+    @staticmethod
+    def _parse_electronic_version(electronic_version):
+        if "file" in electronic_version:
+            url = electronic_version["file"]["url"]
+            file_name = electronic_version["file"]["fileName"]
+            mime_type = electronic_version["file"]["mimeType"]
+        elif "link" in electronic_version:
+            url = electronic_version["link"]
+            file_name = None
+            mime_type = "text/html"
+        else:
+            return
+        access_type = electronic_version.get("accessType", {})
+        access_rights = "ClosedAccess"
+        if access_type.get("uri", "").endswith("/open"):
+            access_rights = "OpenAccess"
+        elif access_type.get("uri", "").endswith("/restricted"):
+            access_rights = "RestrictedAccess"
+        return {
+            "title": file_name,
+            "url": url,
+            "mime_type": mime_type,
+            "hash": sha1(url.encode("utf-8")).hexdigest(),
+            "copyright": None,
+            "access_rights": access_rights
+        }
+
+    @staticmethod
+    def _serialize_access_rights(access_rights):
+        access_rights = access_rights.replace("Access", "")
+        access_rights = access_rights.lower()
+        access_rights += "-access"
+        return access_rights
+
+    @classmethod
+    def get_files(cls, node):
+        electronic_versions = node.get("electronicVersions", []) + node.get("additionalFiles", [])
+        if not electronic_versions:
+            return []
+        return [
+            cls._parse_electronic_version(electronic_version)
+            for electronic_version in electronic_versions if cls._parse_electronic_version(electronic_version)
+        ]
+
+    @classmethod
+    def get_language(cls, node):
+        language = node["language"]["term"]["en_GB"]
+        if language == "Dutch":
+            return "nl"
+        elif language == "English":
+            return "en"
+        return "unk"
+
+    @classmethod
+    def get_url(cls, node):
+        files = cls.get_files(node)
+        if not files:
+            return node["portalUrl"]
+        return files[0]["url"].strip()
+
+    @classmethod
+    def get_mime_type(cls, node):
+        files = cls.get_files(node)
+        if not files:
+            return
+        return files[0]["mime_type"]
+
+    @classmethod
+    def get_technical_type(cls, node):
+        files = cls.get_files(node)
+        if not files:
+            return
+        technical_type = settings.MIME_TYPE_TO_TECHNICAL_TYPE.get(files[0]["mime_type"], None)
+        if technical_type:
+            return technical_type
+        file_url = files[0]["url"]
+        if not file_url:
+            return
+        mime_type, encoding = guess_type(file_url)
+        return settings.MIME_TYPE_TO_TECHNICAL_TYPE.get(mime_type, "unknown")
+
+    @classmethod
+    def get_copyright(cls, node):
+        files = cls.get_files(node)
+        if not len(files):
+            return "closed-access"
+        return cls._serialize_access_rights(files[0]["access_rights"])
+
+    @classmethod
+    def get_from_youtube(cls, node):
+        url = cls.get_url(node)
+        if not url:
+            return False
+        return cls.youtube_regex.match(url) is not None
+
+    @classmethod
+    def get_authors(cls, node):
+        authors = []
+        for person in node["contributors"]:
+            name = person.get('name', {})
+            match name:
+                case {"firstName": first_name}:
+                    full_name = f"{first_name} {name['lastName']}"
+                case {"lastName": last_name}:
+                    full_name = last_name
+                case _:
+                    full_name = None
+            authors.append({
+                "name": full_name,
+                "email": None,
+                "external_id": person["pureId"],
+                "dai": None,
+                "orcid": None,
+                "isni": None
+            })
+        return authors
+
+    @classmethod
+    def get_provider(cls, node):
+        return {
+            "ror": None,
+            "external_id": None,
+            "slug": "edurep",
+            "name": "Edurep"
+        }
+
+    @classmethod
+    def get_organizations(cls, node):
+        root = cls.get_provider(node)
+        root["type"] = "institute"
+        return {
+            "root": root,
+            "departments": [],
+            "associates": []
+        }
+
+    @classmethod
+    def get_publishers(cls, node):
+        return ["Edurep"]
+
+    @classmethod
+    def get_is_restricted(cls, node):
+        return not cls.get_analysis_allowed(node)
+
+    @classmethod
+    def get_analysis_allowed(cls, node):
+        files = cls.get_files(node)
+        if not len(files):
+            return False
+        match files[0]["access_rights"], files[0]["copyright"]:
+            case "OpenAccess", _:
+                return True
+            case "RestrictedAccess", copyright:
+                return copyright and copyright not in ["yes", "unknown"] and "nd" not in copyright
+            case "ClosedAccess", _:
+                return False
+
+    @classmethod
+    def get_doi(cls, node):
+        if "electronicVersions" not in node:
+            return None
+        doi_version = next(
+            (electronic_version for electronic_version in node["electronicVersions"] if "doi" in electronic_version),
+            None
+        )
+        return doi_version["doi"] if doi_version else None
+
+
+EDUREP_EXTRACTION_OBJECTIVE = {
+    # Essential NPPO properties
+    "url": EdurepMetadataExtraction.get_url,
+    "files": EdurepMetadataExtraction.get_files,
+    "copyright": EdurepMetadataExtraction.get_copyright,
+    "title": "$.title.value",
+    "language": EdurepMetadataExtraction.get_language,
+    "keywords": "$.keywordGroups.0.keywords.0.freeKeywords",
+    "description": "$.abstract.en_GB",
+    "mime_type": EdurepMetadataExtraction.get_mime_type,
+    "authors": EdurepMetadataExtraction.get_authors,
+    "organizations": EdurepMetadataExtraction.get_organizations,
+    "publishers": EdurepMetadataExtraction.get_publishers,
+    "publisher_date": lambda node: None,
+    "publisher_year": "$.publicationStatuses.0.publicationDate.year",
+
+    # Non-essential NPPO properties
+    "technical_type": EdurepMetadataExtraction.get_technical_type,
+    "from_youtube": EdurepMetadataExtraction.get_from_youtube,
+    "is_restricted": EdurepMetadataExtraction.get_is_restricted,
+    "analysis_allowed": EdurepMetadataExtraction.get_analysis_allowed,
+    "research_object_type": "$.type.term.en_GB",
+    "research_themes": lambda node: [],
+    "parties": lambda node: [],
+    "doi": EdurepMetadataExtraction.get_doi,
+
+    # Non-essential Edusources properties (for compatibility reasons)
+    "material_types": lambda node: None,
+    "aggregation_level": lambda node: None,
+    "lom_educational_levels": lambda node: [],
+    "studies": lambda node: [],
+    "ideas": lambda node: [],
+    "is_part_of": lambda node: [],
+    "has_parts": lambda node: [],
+    "copyright_description": lambda node: None,
+    "learning_material_disciplines": lambda node: [],
+    "consortium": lambda node: None,
+    "lom_educational_level": lambda node: None,
+    "lowest_educational_level": lambda node: 2,
+}
---
+++ harvester/sources/tests/extraction/test_edurep.py
@@ -0,0 +1,108 @@
+from datetime import datetime
+
+from django.test import TestCase
+from django.utils.timezone import make_aware
+
+from harvester.utils.extraction import get_harvest_seeds
+from core.constants import Repositories
+from sources.factories.edurep.extraction import EdurepPureResourceFactory, SET_SPECIFICATION
+
+
+class TestGetHarvestSeedsEdurep(TestCase):
+
+    begin_of_time = None
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.begin_of_time = make_aware(datetime(year=1970, month=1, day=1))
+        EdurepPureResourceFactory.create_common_responses()
+        cls.seeds = get_harvest_seeds(Repositories.EDUREP_JSONSEARCH, SET_SPECIFICATION, cls.begin_of_time)
+
+    def test_get_id(self):
+        seeds = self.seeds
+        self.assertEqual(seeds[0]["external_id"], "7288bd68-d62b-4db0-8cea-5f189e209254")
+
+#     def test_get_files(self):
+#         seeds = self.seeds
+#         self.assertEqual(seeds[0]["files"], [])
+#         self.assertEqual(seeds[3]["files"], [
+#             {
+#                 "mime_type": "application/pdf",
+#                 "url": "https://accpure.hva.nl/ws/api/research-outputs/d7126f6d-c412-43c8-ad2a-6acb7613917d/files/"
+#                        "MDIyMzRi/636835_schuldenvrij-de-weg-naar-werk_aangepast.pdf",
+#                 "hash": "17c1945a55b6d00b0d17caed17762c94237e658d",
+#                 "title": "636835_schuldenvrij-de-weg-naar-werk_aangepast.pdf",
+#                 "copyright": None,
+#                 "access_rights": "OpenAccess"
+#             }
+#         ])
+#
+#     def test_get_url(self):
+#         seeds = self.seeds
+#         self.assertEqual(
+#             seeds[0]["url"],
+#             "https://accpure.hva.nl/en/publications/7288bd68-d62b-4db0-8cea-5f189e209254"
+#         )
+#         self.assertEqual(
+#             seeds[3]["url"],
+#             "https://accpure.hva.nl/ws/api/research-outputs/d7126f6d-c412-43c8-ad2a-6acb7613917d/files/MDIyMzRi/"
+#             "636835_schuldenvrij-de-weg-naar-werk_aangepast.pdf"
+#         )
+#
+#     def test_get_mime_type(self):
+#         seeds = self.seeds
+#         self.assertIsNone(seeds[0]["mime_type"])
+#         self.assertEqual(seeds[3]["mime_type"], "application/pdf")
+#
+#     def test_get_copyright(self):
+#         seeds = self.seeds
+#         self.assertEqual(seeds[0]["copyright"], "closed-access")
+#         self.assertEqual(seeds[3]["copyright"], "open-access")
+#
+#     def test_get_analysis_allowed(self):
+#         seeds = self.seeds
+#         self.assertFalse(seeds[0]["analysis_allowed"], "Expected closed-access to disallow analysis")
+#         self.assertTrue(seeds[3]["analysis_allowed"], "Expected open-access to allow analysis")
+#
+#     def test_get_language(self):
+#         seeds = self.seeds
+#         self.assertEqual(seeds[0]["language"], {"metadata": "nl"})
+#         self.assertEqual(seeds[4]["language"], {"metadata": "en"})
+#
+#     def test_get_title(self):
+#         seeds = self.seeds
+#         self.assertEqual(seeds[0]["title"], "Leerlingen in het Amsterdamse onderwijs")
+#
+#     def test_get_description(self):
+#         seeds = self.seeds
+#         self.assertTrue(seeds[0]["description"].startswith("De relatie tussen schoolloopbanen van jongeren"))
+#
+#     def test_keywords(self):
+#         seeds = self.seeds
+#         self.assertEqual(seeds[0]["keywords"], ['onderzoek', 'leerlingen', 'Amsterdam', 'schoolloopbanen', 'jongeren'])
+#
+#     def test_authors_property(self):
+#         seeds = self.seeds
+#         self.assertEqual(seeds[0]['authors'], [
+#             {'name': 'Ruben Fukkink', 'email': None, 'external_id': 140056, 'dai': None, 'orcid': None, 'isni': None},
+#             {'name': 'Sandra van Otterloo', 'email': None, 'external_id': 140057, 'dai': None, 'orcid': None, 'isni':
+#                 None},
+#             {'name': 'Lotje Cohen', 'email': None, 'external_id': 140058, 'dai': None, 'orcid': None, 'isni': None},
+#             {'name': 'Merel van der Wouden', 'email': None, 'external_id': 140059, 'dai': None, 'orcid': None, 'isni':
+#                 None},
+#             {'name': 'Bonne Zijlstra', 'email': None, 'external_id': 140060, 'dai': None, 'orcid': None, 'isni': None}
+#         ])
+#
+#     def test_publisher_year(self):
+#         seeds = self.seeds
+#         self.assertEqual(seeds[0]["publisher_year"], 2016)
+#
+#     def test_research_object_type(self):
+#         seeds = self.seeds
+#         self.assertEqual(seeds[0]["research_object_type"], "Report")
+#
+#     def test_doi(self):
+#         seeds = self.seeds
+#         self.assertIsNone(seeds[0]["doi"])
+#         self.assertEqual(seeds[5]["doi"], "10.1088/0031-9120/50/5/573")
