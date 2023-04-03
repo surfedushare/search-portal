1e05dc2bb9c9ebbc254963431344973d2d882171
Kalle Pronk <kallepronk@gmail.com>
2023-04-03 16:40:16 +0200

    Updated edurep resource model

--- harvester/sources/models/edurep.py
+++ harvester/sources/models/edurep.py
@@ -1,4 +1,41 @@
+from django.db import models
+
+from datagrowth.configuration import create_config
+from datagrowth.processors import ExtractProcessor
+
 from core.models import HarvestHttpResource
+from sources.extraction.edurep import EdurepMetadataExtraction, EDUREP_EXTRACTION_OBJECTIVE
+class EdurepJsonSearchResourceManager(models.Manager):
+
+    def extract_seeds(self, latest_update):
+        queryset = self.get_queryset().filter(
+            since__date__gte=latest_update.date(),
+            status=200,
+            is_extracted=False
+        )
+
+        metadata_objective = {
+            "@": "$.items",
+            "external_id": "$.uuid",
+            "state": EdurepMetadataExtraction.get_record_state
+        }
+        metadata_objective.update(EDUREP_EXTRACTION_OBJECTIVE)
+        extract_config = create_config("extract_processor", {
+            "objective": metadata_objective
+        })
+        prc = ExtractProcessor(config=extract_config)
+
+        results = []
+        for harvest in queryset:
+            seed_resource = {
+                "resource": f"{harvest._meta.app_label}.{harvest._meta.model_name}",
+                "id": harvest.id,
+                "success": True
+            }
+            for seed in prc.extract_from_resource(harvest):
+                seed["seed_resource"] = seed_resource
+                results.append(seed)
+        return results
 
 
 class EdurepJsonSearchResource(HarvestHttpResource):
