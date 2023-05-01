51f00a5141447c46873cd4ff304402b59c6b8cb5
Fako Berkers <email@fakoberkers.nl>
2023-05-01 11:54:43 +0200

    Moves all tasks to different queues

--- harvester/harvester/settings/publinova_nl.py
+++ harvester/harvester/settings/publinova_nl.py
@@ -6,6 +6,7 @@ DOCUMENT_TYPE = DocumentTypes.RESEARCH_PRODUCT
 ALLOW_CLOSED_ACCESS_DOCUMENTS = False
 LOWEST_EDUCATIONAL_LEVEL = -1  # will ignore lowest educational level requirements
 
+CELERY_TASK_DEFAULT_QUEUE = "publinova"
 CELERY_TASK_ROUTES = {
     'sync_indices': {'queue': 'publinova-indexing'}
 }
