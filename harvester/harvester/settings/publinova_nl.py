43f1bc95a1b874269f3eaefe5faaafd863b2e2f2
Fako Berkers <email@fakoberkers.nl>
2023-05-04 18:04:05 +0200

    Starts a test with allowing closed-access

--- harvester/harvester/settings/publinova_nl.py
+++ harvester/harvester/settings/publinova_nl.py
@@ -3,5 +3,5 @@ from search_client import DocumentTypes
 
 
 DOCUMENT_TYPE = DocumentTypes.RESEARCH_PRODUCT
-ALLOW_CLOSED_ACCESS_DOCUMENTS = False
+ALLOW_CLOSED_ACCESS_DOCUMENTS = environment.service.env != "production"
 LOWEST_EDUCATIONAL_LEVEL = -1  # will ignore lowest educational level requirements
